c37c744bbb236b1fd39f86e4f0ab36652d403d67
Carl Hetherington <cth@carlh.net>
2012-10-16 16:49:32 +0100

    Fix text in about box.

--- src/tools/dvdomatic.cc
+++ src/tools/dvdomatic.cc
@@ -351,7 +351,7 @@ public:
 	{
 		wxAboutDialogInfo info;
 		info.SetName (_("DVD-o-matic"));
-		info.SetVersion (std_to_wx (String::compose ("DVD-o-matic version %1 git %2", dvdomatic_version, dvdomatic_git_commit)));
+		info.SetVersion (std_to_wx (String::compose ("version %1 git %2", dvdomatic_version, dvdomatic_git_commit)));
 		info.SetDescription (_("Free, open-source DCP generation from almost anything."));
 		info.SetCopyright (_("(C) Carl Hetherington, Terrence Meiczinger, Paul Davis, Ole Laursen"));
 		wxArrayString authors;
