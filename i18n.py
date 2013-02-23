7913cba90bccb9501b63a0518c58abbd5a6b330d
Carl Hetherington <cth@carlh.net>
2013-02-23 00:12:11 +0000

    Try to centralise .pot building.

--- i18n.py
+++ i18n.py
@@ -2,6 +2,17 @@ import glob
 import os
 from waflib import Logs
 
+def pot(dir, sources, name):
+    s = ""
+    for f in sources.split('\n'):
+        t = f.strip()
+        if len(t) > 0:
+            s += (os.path.join(dir, t)) + " "
+
+    Logs.info('Making %s.pot' % os.path.join('build', dir, name))
+    os.system('xgettext -d %s -s --keyword=_ -p %s -o %s.pot %s' % (name, os.path.join('build', dir), name, s))
+    
+
 def po_to_mo(dir, name):
     for f in glob.glob(os.path.join(dir, 'po', '*.po')):
         
