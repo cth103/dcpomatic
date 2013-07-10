9b4910b82a520cf45a4aba6b2486c67524104a8b
Carl Hetherington <cth@carlh.net>
2013-07-10 13:42:02 +0100

    Add missing virtual destructor.

--- src/lib/video_examiner.h
+++ src/lib/video_examiner.h
@@ -24,6 +24,7 @@
 class VideoExaminer
 {
 public:
+	virtual ~VideoExaminer () {}
 	virtual float video_frame_rate () const = 0;
 	virtual libdcp::Size video_size () const = 0;
 	virtual VideoContent::Frame video_length () const = 0;
