5d75984fa7032300481ef0b18f7209053ee53ce4
Carl Hetherington <cth@carlh.net>
2013-07-30 19:01:26 +0100

    Missing OS X dep.

--- platform/osx/make_dmg.sh
+++ platform/osx/make_dmg.sh
@@ -73,6 +73,7 @@ universal_copy $ENV lib/libfreetype*.dylib $WORK/$libs
 universal_copy $ENV lib/libexpat*.dylib $WORK/$libs
 universal_copy $ENV lib/libltdl*.dylib $WORK/$libs
 universal_copy $ENV lib/libxmlsec1*.dylib $WORK/$libs
+universal_copy $ENV lib/libltdl*.dylib $WORK/$libs
 
 for obj in $WORK/$macos/dcpomatic $WORK/$macos/ffprobe $WORK/$libs/*.dylib; do
   deps=`otool -L $obj | awk '{print $1}' | egrep "(/Users/carl|libboost|libssh)"`
