1ba7bca7f2950faa441ec83920d35b65016f3fa6
Carl Hetherington <cth@carlh.net>
2013-09-19 21:39:09 +0100

    Missing file.

---
+++ src/lib/cinema.cc
@@ -0,0 +1,75 @@
+/*
+    Copyright (C) 2013 Carl Hetherington <cth@carlh.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#include <libxml++/libxml++.h>
+#include <libcxml/cxml.h>
+#include "cinema.h"
+
+using std::list;
+using boost::shared_ptr;
+
+Cinema::Cinema (shared_ptr<const cxml::Node> node)
+{
+	name = node->string_child ("Name");
+	email = node->string_child ("Email");
+
+	list<shared_ptr<cxml::Node> > s = node->node_children ("Screen");
+	for (list<shared_ptr<cxml::Node> >::iterator i = s.begin(); i != s.end(); ++i) {
+		_screens.push_back (shared_ptr<Screen> (new Screen (*i)));
+	}
+}
+
+void
+Cinema::as_xml (xmlpp::Element* parent) const
+{
+	parent->add_child("Name")->add_child_text (name);
+	parent->add_child("Email")->add_child_text (email);
+
+	for (list<shared_ptr<Screen> >::const_iterator i = _screens.begin(); i != _screens.end(); ++i) {
+		(*i)->as_xml (parent->add_child ("Screen"));
+	}
+}
+
+void
+Cinema::add_screen (shared_ptr<Screen> s)
+{
+	s->cinema = shared_from_this ();
+	_screens.push_back (s);
+}
+
+void
+Cinema::remove_screen (shared_ptr<Screen> s)
+{
+	_screens.remove (s);
+}
+
+Screen::Screen (shared_ptr<const cxml::Node> node)
+{
+	name = node->string_child ("Name");
+	certificate = shared_ptr<libdcp::Certificate> (new libdcp::Certificate (node->string_child ("Certificate")));
+}
+
+void
+Screen::as_xml (xmlpp::Element* parent) const
+{
+	parent->add_child("Name")->add_child_text (name);
+	parent->add_child("Certificate")->add_child_text (certificate->certificate (true));
+}
+
+		
