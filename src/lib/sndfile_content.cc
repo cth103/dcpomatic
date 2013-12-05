7f2cb335b6d48a23f48761104934e9b06d4ac464
Carl Hetherington <cth@carlh.net>
2013-12-05 23:46:16 +0000

    Fix erroneous full_length() in SndfileContent; it's at least a bit more correct now.

--- src/lib/sndfile_content.cc
+++ src/lib/sndfile_content.cc
@@ -146,8 +146,16 @@ SndfileContent::full_length () const
 {
 	shared_ptr<const Film> film = _film.lock ();
 	assert (film);
+
+	OutputAudioFrame const len = audio_length() * output_audio_frame_rate() / content_audio_frame_rate ();
 	
-	return film->audio_frames_to_time (audio_length ());
+	/* XXX: this depends on whether, alongside this audio, we are running video slower or faster than
+	   it should be.  The calculation above works out the output audio frames assuming that we are just
+	   resampling the audio: it would be incomplete if, for example, we were running this audio alongside
+	   25fps video that was being run at 24fps.
+	*/
+	
+	return film->audio_frames_to_time (len);
 }
 
 int
