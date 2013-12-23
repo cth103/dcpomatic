f051887d714e122565c0b4a129edf0535e4c3706
Carl Hetherington <cth@carlh.net>
2013-12-23 22:12:21 +0000

    Missing files.

---
+++ src/lib/send_kdm_email_job.cc
@@ -0,0 +1,69 @@
+/*
+    Copyright (C) 2013 Carl Hetherington <cth@carlh.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#include "send_kdm_email_job.h"
+#include "compose.hpp"
+#include "film.h"
+#include "kdm.h"
+
+#include "i18n.h"
+
+using std::string;
+using std::list;
+using boost::shared_ptr;
+
+SendKDMEmailJob::SendKDMEmailJob (
+	shared_ptr<const Film> f,
+	list<shared_ptr<Screen> > screens,
+	boost::filesystem::path dcp,
+	boost::posix_time::ptime from,
+	boost::posix_time::ptime to
+	)
+	: Job (f)
+	, _screens (screens)
+	, _dcp (dcp)
+	, _from (from)
+	, _to (to)
+{
+
+}
+
+string
+SendKDMEmailJob::name () const
+{
+	return String::compose (_("Email KDMs for %1"), _film->name());
+}
+
+void
+SendKDMEmailJob::run ()
+{
+	try {
+		
+		set_progress_unknown ();
+		email_kdms (_film, _screens, _dcp, _from, _to);
+		set_progress (1);
+		set_state (FINISHED_OK);
+		
+	} catch (std::exception& e) {
+
+		set_progress (1);
+		set_state (FINISHED_ERROR);
+		throw;
+	}
+}
---
+++ src/lib/send_kdm_email_job.h
@@ -0,0 +1,44 @@
+/*
+    Copyright (C) 2013 Carl Hetherington <cth@carlh.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#include <boost/filesystem.hpp>
+#include "job.h"
+
+class Screen;
+
+class SendKDMEmailJob : public Job
+{
+public:
+	SendKDMEmailJob (
+		boost::shared_ptr<const Film>,
+		std::list<boost::shared_ptr<Screen> >,
+		boost::filesystem::path,
+		boost::posix_time::ptime,
+		boost::posix_time::ptime
+		);
+
+	std::string name () const;
+	void run ();
+
+private:
+	std::list<boost::shared_ptr<Screen> > _screens;
+	boost::filesystem::path _dcp;
+	boost::posix_time::ptime _from;
+	boost::posix_time::ptime _to;
+};
