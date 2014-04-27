bec7e08e29ee49ac2b98dad2bc0edb4ac98319b2
Carl Hetherington <cth@carlh.net>
2014-04-27 22:57:17 +0100

    Copyright date.

--- src/lib/transcoder.cc
+++ src/lib/transcoder.cc
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2012 Carl Hetherington <cth@carlh.net>
+    Copyright (C) 2012-2014 Carl Hetherington <cth@carlh.net>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -35,6 +35,7 @@
 #include "job.h"
 
 using std::string;
+using std::cout;
 using boost::shared_ptr;
 using boost::weak_ptr;
 using boost::dynamic_pointer_cast;
