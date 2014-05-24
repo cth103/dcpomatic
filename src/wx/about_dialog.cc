11f7a81a633f5e17a6870b2cd03d26da056d2529
Carl Hetherington <cth@carlh.net>
2014-05-24 22:51:56 +0100

    Add tester.

--- src/wx/about_dialog.cc
+++ src/wx/about_dialog.cc
@@ -160,6 +160,7 @@ AboutDialog::AboutDialog (wxWindow* parent)
 	tested_by.Add (wxT ("Luke Granger-Brown"));
 	tested_by.Add (wxT ("Steve Guttag"));
 	tested_by.Add (wxT ("Patrick Haderer"));
+	tested_by.Add (wxT ("Bill Hamell"));
 	tested_by.Add (wxT ("Jonathan Jensen"));
 	tested_by.Add (wxT ("Thierry Journet"));
 	tested_by.Add (wxT ("Ada de Kamper"));
