0f28d40f2f41b5a71bc1ed8192ebb575dbd2dc50
Carl Hetherington <cth@carlh.net>
2014-06-10 10:13:35 +0100

    Add test for YUV images encoded by servers.

--- test/client_server_test.cc
+++ test/client_server_test.cc
@@ -42,7 +42,7 @@ do_remote_encode (shared_ptr<DCPVideoFrame> frame, ServerDescription description
 	BOOST_CHECK (memcmp (locally_encoded->data(), remotely_encoded->data(), locally_encoded->size()) == 0);
 }
 
-BOOST_AUTO_TEST_CASE (client_server_test)
+BOOST_AUTO_TEST_CASE (client_server_test_rgb)
 {
 	shared_ptr<Image> image (new Image (PIX_FMT_RGB24, libdcp::Size (1998, 1080), true));
 	uint8_t* p = image->data()[0];
@@ -70,7 +70,86 @@ BOOST_AUTO_TEST_CASE (client_server_test)
 		p += sub_image->stride()[0];
 	}
 
-	shared_ptr<FileLog> log (new FileLog ("build/test/client_server_test.log"));
+	shared_ptr<FileLog> log (new FileLog ("build/test/client_server_test_rgb.log"));
+
+	shared_ptr<PlayerVideoFrame> pvf (
+		new PlayerVideoFrame (
+			shared_ptr<ImageProxy> (new RawImageProxy (image, log)),
+			Crop (),
+			libdcp::Size (1998, 1080),
+			libdcp::Size (1998, 1080),
+			Scaler::from_id ("bicubic"),
+			EYES_BOTH,
+			PART_WHOLE,
+			ColourConversion ()
+			)
+		);
+
+	pvf->set_subtitle (sub_image, Position<int> (50, 60));
+
+	shared_ptr<DCPVideoFrame> frame (
+		new DCPVideoFrame (
+			pvf,
+			0,
+			24,
+			200000000,
+			RESOLUTION_2K,
+			log
+			)
+		);
+
+	shared_ptr<EncodedData> locally_encoded = frame->encode_locally ();
+	BOOST_ASSERT (locally_encoded);
+	
+	Server* server = new Server (log, true);
+
+	new thread (boost::bind (&Server::run, server, 2));
+
+	/* Let the server get itself ready */
+	dcpomatic_sleep (1);
+
+	ServerDescription description ("localhost", 2);
+
+	list<thread*> threads;
+	for (int i = 0; i < 8; ++i) {
+		threads.push_back (new thread (boost::bind (do_remote_encode, frame, description, locally_encoded)));
+	}
+
+	for (list<thread*>::iterator i = threads.begin(); i != threads.end(); ++i) {
+		(*i)->join ();
+	}
+
+	for (list<thread*>::iterator i = threads.begin(); i != threads.end(); ++i) {
+		delete *i;
+	}
+}
+
+BOOST_AUTO_TEST_CASE (client_server_test_yuv)
+{
+	shared_ptr<Image> image (new Image (PIX_FMT_YUV420P, libdcp::Size (1998, 1080), true));
+	uint8_t* p = image->data()[0];
+
+	for (int i = 0; i < image->components(); ++i) {
+		uint8_t* p = image->data()[i];
+		for (int j = 0; j < image->line_size()[i]; ++j) {
+			*p++ = j % 256;
+		}
+	}
+
+	shared_ptr<Image> sub_image (new Image (PIX_FMT_RGBA, libdcp::Size (100, 200), true));
+	p = sub_image->data()[0];
+	for (int y = 0; y < 200; ++y) {
+		uint8_t* q = p;
+		for (int x = 0; x < 100; ++x) {
+			*q++ = y % 256;
+			*q++ = x % 256;
+			*q++ = (x + y) % 256;
+			*q++ = 1;
+		}
+		p += sub_image->stride()[0];
+	}
+
+	shared_ptr<FileLog> log (new FileLog ("build/test/client_server_test_yuv.log"));
 
 	shared_ptr<PlayerVideoFrame> pvf (
 		new PlayerVideoFrame (
