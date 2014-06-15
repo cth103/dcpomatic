19d813fa48ef6f808109969696321f9c902ee63b
Carl Hetherington <cth@carlh.net>
2014-06-15 23:52:41 +0100

    Fix crash on changing audio gain in some locales.
    Reported-by: Daniel Chauvet

--- src/wx/audio_mapping_view.cc
+++ src/wx/audio_mapping_view.cc
@@ -21,6 +21,7 @@
 #include <wx/renderer.h>
 #include <wx/grid.h>
 #include <libdcp/types.h>
+#include <libdcp/raw_convert.h>
 #include "lib/audio_mapping.h"
 #include "lib/util.h"
 #include "audio_mapping_view.h"
@@ -238,7 +239,7 @@ AudioMappingView::update_cells ()
 		_grid->SetCellValue (i, 0, wxString::Format (wxT("%d"), i + 1));
 
 		for (int j = 1; j < _grid->GetNumberCols(); ++j) {
-			_grid->SetCellValue (i, j, std_to_wx (lexical_cast<string> (_map.get (i, static_cast<libdcp::Channel> (j - 1)))));
+			_grid->SetCellValue (i, j, std_to_wx (libdcp::raw_convert<string> (_map.get (i, static_cast<libdcp::Channel> (j - 1)))));
 		}
 	}
 
