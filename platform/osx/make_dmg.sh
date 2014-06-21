ef72df04400ab9ac200756328dc439d8d43366be
Carl Hetherington <cth@carlh.net>
2014-06-21 17:55:39 +0100

    Try to fix OS X / Windows distribution dep.

--- platform/osx/make_dmg.sh
+++ platform/osx/make_dmg.sh
@@ -56,6 +56,7 @@ universal_copy $ROOT lib/libavutil*.dylib $WORK/$libs
 universal_copy $ROOT lib/libavcodec*.dylib $WORK/$libs
 universal_copy $ROOT lib/libswscale*.dylib $WORK/$libs
 universal_copy $ROOT lib/libswresample*.dylib $WORK/$libs
+universal_copy $ROOT lib/libpostproc*.dylib $WORK/$libs
 universal_copy $ROOT bin/ffprobe $WORK/$macos
 universal_copy $ENV lib/libboost_system.dylib $WORK/$libs
 universal_copy $ENV lib/libboost_filesystem.dylib $WORK/$libs
