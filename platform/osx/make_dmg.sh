0c033f4128c003e07fce984b86a840008f31ac6c
Carl Hetherington <cth@carlh.net>
2014-07-22 14:20:28 +0100

    Various OS X build fixes.

--- platform/osx/make_dmg.sh
+++ platform/osx/make_dmg.sh
@@ -15,81 +15,84 @@ ENV=/Users/carl/Environments/osx/10.6
 ROOT=$1
 
 appdir="DCP-o-matic 2.app"
-approot=$appdir/Contents
-libs=$approot/lib
-macos=$approot/MacOS
-resources=$approot/Resources
+approot="$appdir/Contents"
+libs="$approot/lib"
+macos="$approot/MacOS"
+resources="$approot/Resources"
 
-rm -rf $WORK/$appdir
-mkdir -p $WORK/$macos
-mkdir -p $WORK/$libs
-mkdir -p $WORK/$resources
+rm -rf "$WORK/$appdir"
+mkdir -p "$WORK/$macos"
+mkdir -p "$WORK/$libs"
+mkdir -p "$WORK/$resources"
 
 function universal_copy {
     echo $2
     for f in $1/32/$2; do
         if [ -h $f ]; then
-	    ln -s $(readlink $f) $3/`basename $f`
+	    ln -s $(readlink $f) "$3/`basename $f`"
         else
           g=`echo $f | sed -e "s/\/32\//\/64\//g"`
 	  mkdir -p $3
-          lipo -create $f $g -output $3/`basename $f`
+	  echo "lipo -create $f $g -output $3/`basename $f`"
+          lipo -create $f $g -output "$3/`basename $f`"
         fi
     done
 }
 
-universal_copy $ROOT src/dcpomatic/build/src/tools/dcpomatic2 $WORK/$macos
-universal_copy $ROOT src/dcpomatic/build/src/tools/dcpomatic2_cli $WORK/$macos
-universal_copy $ROOT src/dcpomatic/build/src/tools/dcpomatic2_server_cli $WORK/$macos
-universal_copy $ROOT src/dcpomatic/build/src/tools/dcpomatic2_batch $WORK/$macos
-universal_copy $ROOT src/dcpomatic/build/src/lib/libdcpomatic2.dylib $WORK/$libs
-universal_copy $ROOT src/dcpomatic/build/src/wx/libdcpomatic2-wx.dylib $WORK/$libs
-universal_copy $ROOT lib/libcxml.dylib $WORK/$libs
-universal_copy $ROOT lib/libdcp-1.0.dylib $WORK/$libs
-universal_copy $ROOT lib/libasdcp-libdcp-1.0.dylib $WORK/$libs
-universal_copy $ROOT lib/libkumu-libdcp-1.0.dylib $WORK/$libs
-universal_copy $ROOT lib/libopenjpeg*.dylib $WORK/$libs
-universal_copy $ROOT lib/libavdevice*.dylib $WORK/$libs
-universal_copy $ROOT lib/libavformat*.dylib $WORK/$libs
-universal_copy $ROOT lib/libavfilter*.dylib $WORK/$libs
-universal_copy $ROOT lib/libavutil*.dylib $WORK/$libs
-universal_copy $ROOT lib/libavcodec*.dylib $WORK/$libs
-universal_copy $ROOT lib/libswscale*.dylib $WORK/$libs
-universal_copy $ROOT lib/libswresample*.dylib $WORK/$libs
-universal_copy $ROOT lib/libpostproc*.dylib $WORK/$libs
-universal_copy $ROOT bin/ffprobe $WORK/$macos
-universal_copy $ENV lib/libboost_system.dylib $WORK/$libs
-universal_copy $ENV lib/libboost_filesystem.dylib $WORK/$libs
-universal_copy $ENV lib/libboost_thread.dylib $WORK/$libs
-universal_copy $ENV lib/libboost_date_time.dylib $WORK/$libs
-universal_copy $ENV lib/libxml++-2.6*.dylib $WORK/$libs
-universal_copy $ENV lib/libxml2*.dylib $WORK/$libs
-universal_copy $ENV lib/libglibmm-2.4*.dylib $WORK/$libs
-universal_copy $ENV lib/libgobject*.dylib $WORK/$libs
-universal_copy $ENV lib/libgthread*.dylib $WORK/$libs
-universal_copy $ENV lib/libgmodule*.dylib $WORK/$libs
-universal_copy $ENV lib/libsigc*.dylib $WORK/$libs
-universal_copy $ENV lib/libglib-2*.dylib $WORK/$libs
-universal_copy $ENV lib/libintl*.dylib $WORK/$libs
-universal_copy $ENV lib/libsndfile*.dylib $WORK/$libs
-universal_copy $ENV lib/libMagick++*.dylib $WORK/$libs
-universal_copy $ENV lib/libMagickCore*.dylib $WORK/$libs
-universal_copy $ENV lib/libMagickWand*.dylib $WORK/$libs
-universal_copy $ENV lib/libssh*.dylib $WORK/$libs
-universal_copy $ENV lib/libwx*.dylib $WORK/$libs
-universal_copy $ENV lib/libfontconfig*.dylib $WORK/$libs
-universal_copy $ENV lib/libfreetype*.dylib $WORK/$libs
-universal_copy $ENV lib/libexpat*.dylib $WORK/$libs
-universal_copy $ENV lib/libltdl*.dylib $WORK/$libs
-universal_copy $ENV lib/libxmlsec1*.dylib $WORK/$libs
-universal_copy $ENV lib/libzip*.dylib $WORK/$libs
-universal_copy $ENV lib/libquickmail*.dylib $WORK/$libs
-universal_copy $ENV lib/libcurl*.dylib $WORK/$libs
-universal_copy $ENV lib/libffi*.dylib $WORK/$libs
-universal_copy $ENV lib/libiconv*.dylib $WORK/$libs
+universal_copy $ROOT src/dcpomatic/build/src/tools/dcpomatic2 "$WORK/$macos"
+universal_copy $ROOT src/dcpomatic/build/src/tools/dcpomatic2_cli "$WORK/$macos"
+universal_copy $ROOT src/dcpomatic/build/src/tools/dcpomatic2_server_cli "$WORK/$macos"
+universal_copy $ROOT src/dcpomatic/build/src/tools/dcpomatic2_batch "$WORK/$macos"
+universal_copy $ROOT src/dcpomatic/build/src/lib/libdcpomatic2.dylib "$WORK/$libs"
+universal_copy $ROOT src/dcpomatic/build/src/wx/libdcpomatic2-wx.dylib "$WORK/$libs"
+universal_copy $ROOT lib/libcxml.dylib "$WORK/$libs"
+universal_copy $ROOT lib/libdcp-1.0.dylib "$WORK/$libs"
+universal_copy $ROOT lib/libasdcp-libdcp-1.0.dylib "$WORK/$libs"
+universal_copy $ROOT lib/libkumu-libdcp-1.0.dylib "$WORK/$libs"
+universal_copy $ROOT lib/libopenjpeg*.dylib "$WORK/$libs"
+universal_copy $ROOT lib/libavdevice*.dylib "$WORK/$libs"
+universal_copy $ROOT lib/libavformat*.dylib "$WORK/$libs"
+universal_copy $ROOT lib/libavfilter*.dylib "$WORK/$libs"
+universal_copy $ROOT lib/libavutil*.dylib "$WORK/$libs"
+universal_copy $ROOT lib/libavcodec*.dylib "$WORK/$libs"
+universal_copy $ROOT lib/libswscale*.dylib "$WORK/$libs"
+universal_copy $ROOT lib/libswresample*.dylib "$WORK/$libs"
+universal_copy $ROOT lib/libpostproc*.dylib "$WORK/$libs"
+universal_copy $ROOT bin/ffprobe "$WORK/$macos"
+universal_copy $ENV lib/libboost_system.dylib "$WORK/$libs"
+universal_copy $ENV lib/libboost_filesystem.dylib "$WORK/$libs"
+universal_copy $ENV lib/libboost_thread.dylib "$WORK/$libs"
+universal_copy $ENV lib/libboost_date_time.dylib "$WORK/$libs"
+universal_copy $ENV lib/libxml++-2.6*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libxml2*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libglibmm-2.4*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libgobject*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libgthread*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libgmodule*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libsigc*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libglib-2*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libintl*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libsndfile*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libMagick++*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libMagickCore*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libMagickWand*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libssh*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libwx*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libfontconfig*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libfreetype*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libexpat*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libltdl*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libxmlsec1*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libzip*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libquickmail*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libcurl*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libffi*.dylib "$WORK/$libs"
+universal_copy $ENV lib/libiconv*.dylib "$WORK/$libs"
 
-for obj in $WORK/$macos/dcpomatic2 $WORK/$macos/dcpomatic2_batch $WORK/$macos/dcpomatic2_cli $WORK/$macos/dcpomatic2_server_cli $WORK/$macos/ffprobe $WORK/$libs/*.dylib; do
-  deps=`otool -L $obj | awk '{print $1}' | egrep "(/Users/carl|libboost|libssh|libltdl|libxmlsec)"`
+for obj in "$WORK/$macos/dcpomatic2" "$WORK/$macos/dcpomatic2_batch" "$WORK/$macos/dcpomatic2_cli" "$WORK/$macos/dcpomatic2_server_cli" "$WORK/$macos/ffprobe" "$WORK/$libs/"*.dylib; do
+  echo $WORK
+  echo $obj
+  deps=`otool -L "$obj" | awk '{print $1}' | egrep "(/Users/carl|libboost|libssh|libltdl|libxmlsec)"`
   changes=""
   for dep in $deps; do
     base=`basename $dep`
@@ -98,31 +101,31 @@ for obj in $WORK/$macos/dcpomatic2 $WORK/$macos/dcpomatic2_batch $WORK/$macos/dc
     changes="$changes -change $dep @executable_path/../lib/$base -change $dep32 @executable_path/../lib/$base"
   done
   if test "x$changes" != "x"; then
-    install_name_tool $changes $obj
+    install_name_tool $changes "$obj"
   fi
 done
 
-cp build/platform/osx/Info.plist $WORK/$approot
-cp icons/dcpomatic.icns $WORK/$resources/DCP-o-matic.icns
-cp icons/colour_conversions.png $WORK/$resources
-cp icons/defaults.png $WORK/$resources
-cp icons/kdm_email.png $WORK/$resources
-cp icons/servers.png $WORK/$resources
-cp icons/tms.png $WORK/$resources
-cp icons/keys.png $WORK/$resources
+cp build/platform/osx/Info.plist "$WORK/$approot"
+cp icons/dcpomatic.icns "$WORK/$resources/DCP-o-matic.icns"
+cp icons/colour_conversions.png "$WORK/$resources"
+cp icons/defaults.png "$WORK/$resources"
+cp icons/kdm_email.png "$WORK/$resources"
+cp icons/servers.png "$WORK/$resources"
+cp icons/tms.png "$WORK/$resources"
+cp icons/keys.png "$WORK/$resources"
 
 # i18n: DCP-o-matic .mo files
 for lang in de_DE es_ES fr_FR it_IT sv_SE nl_NL; do
-  mkdir $WORK/$resources/$lang
-  cp build/src/lib/mo/$lang/*.mo $WORK/$resources/$lang
-  cp build/src/wx/mo/$lang/*.mo $WORK/$resources/$lang
-  cp build/src/tools/mo/$lang/*.mo $WORK/$resources/$lang
+  mkdir "$WORK/$resources/$lang"
+  cp build/src/lib/mo/$lang/*.mo "$WORK/$resources/$lang"
+  cp build/src/wx/mo/$lang/*.mo "$WORK/$resources/$lang"
+  cp build/src/tools/mo/$lang/*.mo "$WORK/$resources/$lang"
 done
 
 # i18n: wxWidgets .mo files
 for lang in de es fr it sv nl; do
-  mkdir $WORK/$resources/$lang
-  cp $ENV/64/share/locale/$lang/LC_MESSAGES/wxstd.mo $WORK/$resources/$lang
+  mkdir "$WORK/$resources/$lang"
+  cp $ENV/64/share/locale/$lang/LC_MESSAGES/wxstd.mo "$WORK/$resources/$lang"
 done
 
 tmp_dmg=$WORK/dcpomatic_tmp.dmg
@@ -130,8 +133,8 @@ dmg="$WORK/DCP-o-matic $version.dmg"
 vol_name=DCP-o-matic-$version
 
 mkdir -p $WORK/$vol_name
-cp -r $WORK/$appdir $WORK/$vol_name
-ln -s /Applications $WORK/$vol_name/Applications
+cp -r "$WORK/$appdir" $WORK/$vol_name
+ln -s /Applications "$WORK/$vol_name/Applications"
 
 rm -f $tmp_dmg "$dmg"
 hdiutil create -srcfolder $WORK/$vol_name -volname $vol_name -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDRW -size $DMG_SIZE $tmp_dmg
@@ -160,13 +163,12 @@ echo '
    end tell
 ' | osascript
 
-chmod -Rf go-w /Volumes/"$vol_name"/$appdir
+chmod -Rf go-w /Volumes/"$vol_name"/"$appdir"
 sync
 
 hdiutil eject $device
 hdiutil convert -format UDZO $tmp_dmg -imagekey zlib-level=9 -o "$dmg"
-sips -i $WORK/$resources/DCP-o-matic.icns
-DeRez -only icns $WORK/$resources/DCP-o-matic.icns > $WORK/$resources/DCP-o-matic.rsrc
-Rez -append $WORK/$resources/DCP-o-matic.rsrc -o "$dmg"
+sips -i "$WORK/$resources/DCP-o-matic.icns"
+DeRez -only icns "$WORK/$resources/DCP-o-matic.icns" > "$WORK/$resources/DCP-o-matic.rsrc"
+Rez -append "$WORK/$resources/DCP-o-matic.rsrc" -o "$dmg"
 SetFile -a C "$dmg"
-
