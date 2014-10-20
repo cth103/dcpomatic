e74b8e1d737deb9979a279d94c909d95f9999995
Carl Hetherington <cth@carlh.net>
2014-10-20 22:39:16 +0100

    Data must be passed to libdcp xyz conversion as little-endian.

--- src/lib/dcp_video_frame.cc
+++ src/lib/dcp_video_frame.cc
@@ -125,7 +125,7 @@ DCPVideoFrame::encode_locally ()
 	}
 
 	shared_ptr<libdcp::XYZFrame> xyz = libdcp::rgb_to_xyz (
-		_frame->image(AV_PIX_FMT_RGB48BE),
+		_frame->image(AV_PIX_FMT_RGB48LE),
 		in_lut,
 		libdcp::GammaLUT::cache.get (16, 1 / _frame->colour_conversion().output_gamma),
 		matrix
