e3ae1f787a67296c805d39234669aa8f66f95d27
Carl Hetherington <cth@carlh.net>
2014-11-21 09:27:06 +0000

    Fix recover test filename.

--- test/recover_test.cc
+++ test/recover_test.cc
@@ -57,7 +57,7 @@ BOOST_AUTO_TEST_CASE (recover_test)
 	film->make_dcp ();
 	wait_for_jobs ();
 
-	boost::filesystem::path const video = "build/test/recover_test/video/185_2K_1133fd57e751ce3e82146492466365f9_24_bicubic_100000000_P_S_3D.mxf";
+	boost::filesystem::path const video = "build/test/recover_test/video/185_2K_1918f67fe87ecfc4e8c09fe1c2dcefcc_24_bicubic_100000000_P_S_3D.mxf";
 
 	boost::filesystem::copy_file (
 		video,
