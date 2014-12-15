d7bffb968f0abd5c1e6d5e72e6ea70a701fe5d48
Carl Hetherington <cth@carlh.net>
2014-12-15 11:01:40 +0000

    Hand-apply d1bffe67820adaa10654f722b15eebdd773b3680; remove unnecessary JobWrapper stuff.

--- src/tools/dcpomatic.cc
+++ src/tools/dcpomatic.cc
@@ -36,7 +36,6 @@
 #include "wx/film_editor.h"
 #include "wx/job_manager_view.h"
 #include "wx/config_dialog.h"
-#include "wx/job_wrapper.h"
 #include "wx/wx_util.h"
 #include "wx/new_film_dialog.h"
 #include "wx/properties_dialog.h"
@@ -399,8 +398,14 @@ private:
 				return;
 			}
 		}
-		
-		JobWrapper::make_dcp (this, _film);
+
+		try {
+			_film->make_dcp ();
+		} catch (BadSettingError& e) {
+			error_dialog (this, wxString::Format (_("Bad setting for %s (%s)"), std_to_wx(e.setting()).data(), std_to_wx(e.what()).data()));
+		} catch (std::exception& e) {
+			error_dialog (this, wxString::Format (_("Could not make DCP: %s"), std_to_wx(e.what()).data()));
+		}
 	}
 
 	void jobs_make_kdms ()
