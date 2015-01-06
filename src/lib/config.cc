f7fff16bad3e5da76f37f004d50649350d7bbb25
Carl Hetherington <cth@carlh.net>
2015-01-06 15:58:34 +0000

    Default to logging at DEBUG level.

--- src/lib/config.cc
+++ src/lib/config.cc
@@ -73,7 +73,7 @@ Config::Config ()
 	, _check_for_updates (false)
 	, _check_for_test_updates (false)
 	, _maximum_j2k_bandwidth (250000000)
-	, _log_types (Log::TYPE_GENERAL | Log::TYPE_WARNING | Log::TYPE_ERROR)
+	, _log_types (Log::TYPE_GENERAL | Log::TYPE_WARNING | Log::TYPE_ERROR | Log::TYPE_DEBUG)
 #ifdef DCPOMATIC_WINDOWS	  
 	, _win32_console (false)
 #endif	  
