2e9855c34f861ed59affb0cfc021f78ea5417808
Carl Hetherington <cth@carlh.net>
2015-01-25 15:00:01 +0000

    Fix test filename after digest changes.

--- test/recover_test.cc
+++ test/recover_test.cc
@@ -57,7 +57,7 @@ BOOST_AUTO_TEST_CASE (recover_test)
 	film->make_dcp ();
 	wait_for_jobs ();
 
-	boost::filesystem::path const video = "build/test/recover_test/video/185_2K_80b0300a477c0792f2d9386482cd2ad5_24_bicubic_100000000_P_S_3D.mxf";
+	boost::filesystem::path const video = "build/test/recover_test/video/185_2K_e01c58c5783da57b689e65d9123ad3a3_24_bicubic_100000000_P_S_3D.mxf";
 
 	boost::filesystem::copy_file (
 		video,
