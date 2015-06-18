24947de661db9a5c637abfd6202d5a20cb9ca166
Carl Hetherington <cth@carlh.net>
2015-06-18 19:30:28 +0100

    Update test after change to video identifiers.

--- test/recover_test.cc
+++ test/recover_test.cc
@@ -57,7 +57,7 @@ BOOST_AUTO_TEST_CASE (recover_test)
 	film->make_dcp ();
 	wait_for_jobs ();
 
-	boost::filesystem::path const video = "build/test/recover_test/video/185_2K_40c62abb30c0c1185678d71d58674459_24_100000000_P_S_3D.mxf";
+	boost::filesystem::path const video = "build/test/recover_test/video/185_2K_0e7cded8bd5cb8143184fc7729be136e_24_100000000_P_S_3D.mxf";
 
 	boost::filesystem::copy_file (
 		video,
