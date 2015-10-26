a9f8daff61e227f046f5b87830ce902fd9c3fed7
Carl Hetherington <cth@carlh.net>
2015-10-26 13:03:14 +0000

    Speculative fix for image corruption following 2e62b2c0de47fc8e5bcb5466b2876f9b23d2ae84

--- src/lib/ffmpeg_decoder.cc
+++ src/lib/ffmpeg_decoder.cc
@@ -116,7 +116,7 @@ FFmpegDecoder::pass (PassReason reason, bool accurate)
 		decode_video_packet ();
 	} else if (fc->subtitle_stream() && fc->subtitle_stream()->uses_index (_format_context, si)) {
 		decode_subtitle_packet ();
-	} else if (reason != PASS_REASON_SUBTITLE) {
+	} else if (accurate || reason != PASS_REASON_SUBTITLE) {
 		decode_audio_packet ();
 	}
 
