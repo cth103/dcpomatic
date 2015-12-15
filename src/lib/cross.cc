7a637cbe28de52ae1f5b8726e241a3ee0c5125d4
Carl Hetherington <cth@carlh.net>
2015-12-15 21:26:21 +0000

    Debug batch converter startup.

--- src/lib/cross.cc
+++ src/lib/cross.cc
@@ -376,6 +376,7 @@ start_batch_converter (boost::filesystem::path dcpomatic)
 #if defined(DCPOMATIC_LINUX) || defined(DCPOMATIC_OSX)
 	pid_t pid = fork ();
 	if (pid == 0) {
+		std::cout << "start " << batch << "\n";
 		int const r = system (batch.string().c_str());
 		exit (WEXITSTATUS (r));
 	}
