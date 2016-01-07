08196a1c571a0023a3c3142163a5745a37564572
Carl Hetherington <cth@carlh.net>
2016-01-07 18:40:06 +0000

    Make finder window a bit wider in the .dmg.

--- platform/osx/make_dmg.sh
+++ platform/osx/make_dmg.sh
@@ -265,7 +265,7 @@ echo '
            set current view of container window to icon view
            set toolbar visible of container window to false
            set statusbar visible of container window to false
-           set the bounds of container window to {400, 200, 890, 610}
+           set the bounds of container window to {400, 200, 930, 610}
            set theViewOptions to the icon view options of container window
            set arrangement of theViewOptions to not arranged
            set icon size of theViewOptions to 64
