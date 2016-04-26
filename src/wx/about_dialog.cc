d923dd31c5378d41e80b9cffb985f1a38a431f98
Carl Hetherington <cth@carlh.net>
2016-04-26 15:19:28 +0100

    Supporter.

--- src/wx/about_dialog.cc
+++ src/wx/about_dialog.cc
@@ -147,6 +147,7 @@ AboutDialog::AboutDialog (wxWindow* parent)
 	supported_by.Add (wxT ("Adam Colt"));
 	supported_by.Add (wxT ("Adam Cousins"));
 	supported_by.Add (wxT ("Matthias Damm"));
+	supported_by.Add (wxT ("JP Davidson"));
 	supported_by.Add (wxT ("Yohann Dedy"));
 	supported_by.Add (wxT ("Alexey Derevyanko"));
 	supported_by.Add (wxT ("Thomas Dickens"));
