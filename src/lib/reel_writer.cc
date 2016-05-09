9fe5ea488461355a779db1b6b56f93ef375bac41
Carl Hetherington <cth@carlh.net>
2016-05-09 23:39:42 +0100

    Temporary hack to double-check existing frame hashes.

--- src/lib/reel_writer.cc
+++ src/lib/reel_writer.cc
@@ -200,7 +200,7 @@ ReelWriter::check_existing_picture_asset ()
 		_first_nonexistant_frame = n;
 	}
 
-	while (!existing_picture_frame_ok(asset_file, info_file) && _first_nonexistant_frame > 0) {
+	while (!existing_picture_frame_ok(asset_file, info_file, _first_nonexistant_frame) && _first_nonexistant_frame > 0) {
 		--_first_nonexistant_frame;
 	}
 
@@ -209,6 +209,12 @@ ReelWriter::check_existing_picture_asset ()
 		   do this if we're in 2D and we've just found a good B(oth) frame.
 		*/
 		++_first_nonexistant_frame;
+	}
+
+	for (int i = 0; i < _first_nonexistant_frame; ++i) {
+		if (!existing_picture_frame_ok(asset_file, info_file, i)) {
+			LOG_GENERAL ("Although %1 is not ok", i);
+		}
 	}
 
 	LOG_GENERAL ("Proceeding with first nonexistant frame %1", _first_nonexistant_frame);
@@ -472,14 +478,14 @@ ReelWriter::write (PlayerSubtitles subs)
 }
 
 bool
-ReelWriter::existing_picture_frame_ok (FILE* asset_file, FILE* info_file) const
+ReelWriter::existing_picture_frame_ok (FILE* asset_file, FILE* info_file, int frame) const
 {
-	LOG_GENERAL ("Checking existing picture frame %1", _first_nonexistant_frame);
+	LOG_GENERAL ("Checking existing picture frame %1", frame);
 
 	/* Read the data from the info file; for 3D we just check the left
 	   frames until we find a good one.
 	*/
-	dcp::FrameInfo const info = read_frame_info (info_file, _first_nonexistant_frame, _film->three_d () ? EYES_LEFT : EYES_BOTH);
+	dcp::FrameInfo const info = read_frame_info (info_file, frame, _film->three_d () ? EYES_LEFT : EYES_BOTH);
 
 	bool ok = true;
 
@@ -489,14 +495,14 @@ ReelWriter::existing_picture_frame_ok (FILE* asset_file, FILE* info_file) const
 	size_t const read = fread (data.data().get(), 1, data.size(), asset_file);
 	LOG_GENERAL ("Read %1 bytes of asset data; wanted %2", read, info.size);
 	if (read != static_cast<size_t> (data.size ())) {
-		LOG_GENERAL ("Existing frame %1 is incomplete", _first_nonexistant_frame);
+		LOG_GENERAL ("Existing frame %1 is incomplete", frame);
 		ok = false;
 	} else {
 		MD5Digester digester;
 		digester.add (data.data().get(), data.size());
 		LOG_GENERAL ("Hash %1 vs %2", digester.get(), info.hash);
 		if (digester.get() != info.hash) {
-			LOG_GENERAL ("Existing frame %1 failed hash check", _first_nonexistant_frame);
+			LOG_GENERAL ("Existing frame %1 failed hash check", frame);
 			ok = false;
 		}
 	}
