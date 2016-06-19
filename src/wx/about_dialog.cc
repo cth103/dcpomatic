cc881538178e8efd18213e45504fe0acb9789ac6
Carl Hetherington <cth@carlh.net>
2016-06-19 21:23:48 +0100

    Updated pt_BR translation from Max M. Fuhlendorf.

--- src/wx/about_dialog.cc
+++ src/wx/about_dialog.cc
@@ -107,6 +107,7 @@ AboutDialog::AboutDialog (wxWindow* parent)
 	translated_by.Add (wxT ("Massimiliano Broggi"));
 	translated_by.Add (wxT ("Davide Dall'AraCiao"));
 	translated_by.Add (wxT ("William Fanelli"));
+	translated_by.Add (wxT ("Max M. Fuhlendorf"));
 	translated_by.Add (wxT ("Tomáš Hlaváč"));
 	translated_by.Add (wxT ("Thierry Journet"));
 	translated_by.Add (wxT ("Adam Klotblixt"));
