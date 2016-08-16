fdcce9bdc44b246151ffe1aa6250f456e41d9c3d
Carl Hetherington <cth@carlh.net>
2016-08-16 23:35:41 +0100

    Add a basic test for OV/VF with subs (#928).

--- test/vf_test.cc
+++ test/vf_test.cc
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2015 Carl Hetherington <cth@carlh.net>
+    Copyright (C) 2015-2016 Carl Hetherington <cth@carlh.net>
 
     This file is part of DCP-o-matic.
 
@@ -21,13 +21,21 @@
 #include "lib/film.h"
 #include "lib/dcp_content.h"
 #include "lib/ffmpeg_content.h"
+#include "lib/content_factory.h"
+#include "lib/dcp_content_type.h"
+#include "lib/video_content.h"
 #include "test.h"
+#include <dcp/cpl.h>
+#include <dcp/reel.h>
+#include <dcp/reel_picture_asset.h>
+#include <dcp/reel_sound_asset.h>
 #include <boost/test/unit_test.hpp>
 #include <boost/foreach.hpp>
 
 using std::list;
 using std::string;
 using boost::shared_ptr;
+using boost::dynamic_pointer_cast;
 
 /** Test the logic which decides whether a DCP can be referenced or not */
 BOOST_AUTO_TEST_CASE (vf_test1)
@@ -67,4 +75,59 @@ BOOST_AUTO_TEST_CASE (vf_test1)
 	BOOST_CHECK (dcp->can_reference_audio(why_not));
 	/* (reels_test2 has no subtitles to reference) */
 	BOOST_CHECK (!dcp->can_reference_subtitle(why_not));
+}
+
+/** Make a OV with video and audio and a VF referencing the OV and adding subs */
+BOOST_AUTO_TEST_CASE (vf_test2)
+{
+	/* Make the OV */
+	shared_ptr<Film> ov = new_test_film ("vf_test2_ov");
+	ov->set_dcp_content_type (DCPContentType::from_isdcf_name ("TST"));
+	ov->set_name ("vf_test2_ov");
+	shared_ptr<Content> video = content_factory (ov, "test/data/flat_red.png");
+	ov->examine_and_add_content (video);
+	wait_for_jobs ();
+	video->video->set_length (24 * 5);
+	shared_ptr<Content> audio = content_factory (ov, "test/data/white.wav");
+	ov->examine_and_add_content (audio);
+	wait_for_jobs ();
+	ov->make_dcp ();
+	wait_for_jobs ();
+
+	/* Make the VF */
+	shared_ptr<Film> vf = new_test_film ("vf_test2_vf");
+	vf->set_name ("vf_test2_vf");
+	vf->set_dcp_content_type (DCPContentType::from_isdcf_name ("TST"));
+	vf->set_reel_type (REELTYPE_BY_VIDEO_CONTENT);
+	shared_ptr<DCPContent> dcp = dynamic_pointer_cast<DCPContent> (content_factory (vf, ov->dir (ov->dcp_name ())));
+	BOOST_REQUIRE (dcp);
+	vf->examine_and_add_content (dcp);
+	wait_for_jobs ();
+	dcp->set_reference_video (true);
+	dcp->set_reference_audio (true);
+	shared_ptr<Content> sub = content_factory (vf, "test/data/subrip4.srt");
+	vf->examine_and_add_content (sub);
+	vf->make_dcp ();
+	wait_for_jobs ();
+	vf->write_metadata ();
+
+	dcp::DCP ov_c (ov->dir (ov->dcp_name ()));
+	ov_c.read ();
+	BOOST_REQUIRE_EQUAL (ov_c.cpls().size(), 1);
+	BOOST_REQUIRE_EQUAL (ov_c.cpls().front()->reels().size(), 1);
+	BOOST_REQUIRE (ov_c.cpls().front()->reels().front()->main_picture());
+	string const pic_id = ov_c.cpls().front()->reels().front()->main_picture()->id();
+	BOOST_REQUIRE (ov_c.cpls().front()->reels().front()->main_sound());
+	string const sound_id = ov_c.cpls().front()->reels().front()->main_sound()->id();
+	BOOST_REQUIRE (!ov_c.cpls().front()->reels().front()->main_subtitle());
+
+	dcp::DCP vf_c (vf->dir (vf->dcp_name ()));
+	vf_c.read ();
+	BOOST_REQUIRE_EQUAL (vf_c.cpls().size(), 1);
+	BOOST_REQUIRE_EQUAL (vf_c.cpls().front()->reels().size(), 1);
+	BOOST_REQUIRE (vf_c.cpls().front()->reels().front()->main_picture());
+	BOOST_CHECK_EQUAL (vf_c.cpls().front()->reels().front()->main_picture()->id(), pic_id);
+	BOOST_REQUIRE (vf_c.cpls().front()->reels().front()->main_sound());
+	BOOST_CHECK_EQUAL (vf_c.cpls().front()->reels().front()->main_sound()->id(), sound_id);
+	BOOST_REQUIRE (vf_c.cpls().front()->reels().front()->main_subtitle());
 }
