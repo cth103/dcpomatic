edb7b4d0a662f8d2ed0218194179249d96d13103
Carl Hetherington <cth@carlh.net>
2017-02-28 11:39:26 +0000

    Restore player_test and move black_fill_test into it.

--- test/player_test.cc
+++ test/player_test.cc
@@ -30,11 +30,14 @@
 #include "lib/ratio.h"
 #include "lib/audio_buffers.h"
 #include "lib/player.h"
+#include "lib/video_content.h"
+#include "lib/image_content.h"
 #include "test.h"
 
 using std::cout;
 using std::list;
 using boost::shared_ptr;
+using boost::bind;
 
 static bool
 valid (Content const *)
@@ -87,6 +90,16 @@ BOOST_AUTO_TEST_CASE (player_overlaps_test)
 	BOOST_CHECK_EQUAL (o.front()->content, B);
 }
 
+static shared_ptr<AudioBuffers> accumulated;
+
+static void
+accumulate (shared_ptr<AudioBuffers> audio, DCPTime t)
+{
+	cout << "got " << audio->frames() << " @ " << to_string(t) << "\n";
+	BOOST_REQUIRE (accumulated);
+	accumulated->append (audio);
+}
+
 /** Check that the Player correctly generates silence when used with a silent FFmpegContent */
 BOOST_AUTO_TEST_CASE (player_silence_padding_test)
 {
@@ -99,14 +112,57 @@ BOOST_AUTO_TEST_CASE (player_silence_padding_test)
 	film->examine_and_add_content (c);
 	wait_for_jobs ();
 
-	shared_ptr<Player> player (new Player (film, film->playlist ()));
-	shared_ptr<AudioBuffers> test = player->get_audio (DCPTime (0), DCPTime::from_seconds (1), true);
-	BOOST_CHECK_EQUAL (test->frames(), 48000);
-	BOOST_CHECK_EQUAL (test->channels(), film->audio_channels ());
+	accumulated.reset (new AudioBuffers (film->audio_channels(), 0));
 
-	for (int i = 0; i < test->frames(); ++i) {
-		for (int c = 0; c < test->channels(); ++c) {
-			BOOST_CHECK_EQUAL (test->data()[c][i], 0);
+	shared_ptr<Player> player (new Player (film, film->playlist ()));
+	player->Audio.connect (bind (&accumulate, _1, _2));
+	while (!player->pass ()) {}
+	BOOST_REQUIRE (accumulated->frames() >= 48000);
+	BOOST_CHECK_EQUAL (accumulated->channels(), film->audio_channels ());
+
+	for (int i = 0; i < 48000; ++i) {
+		for (int c = 0; c < accumulated->channels(); ++c) {
+			BOOST_CHECK_EQUAL (accumulated->data()[c][i], 0);
 		}
 	}
+}
+
+/* Test insertion of black frames between separate bits of video content */
+BOOST_AUTO_TEST_CASE (player_black_fill_test)
+{
+	shared_ptr<Film> film = new_test_film ("black_fill_test");
+	film->set_dcp_content_type (DCPContentType::from_isdcf_name ("FTR"));
+	film->set_name ("black_fill_test");
+	film->set_container (Ratio::from_id ("185"));
+	film->set_sequence (false);
+	shared_ptr<ImageContent> contentA (new ImageContent (film, "test/data/simple_testcard_640x480.png"));
+	shared_ptr<ImageContent> contentB (new ImageContent (film, "test/data/simple_testcard_640x480.png"));
+
+	film->examine_and_add_content (contentA);
+	film->examine_and_add_content (contentB);
+	wait_for_jobs ();
+
+	contentA->video->set_scale (VideoContentScale (Ratio::from_id ("185")));
+	contentA->video->set_length (3);
+	contentA->set_position (DCPTime::from_frames (2, film->video_frame_rate ()));
+	contentB->video->set_scale (VideoContentScale (Ratio::from_id ("185")));
+	contentB->video->set_length (1);
+	contentB->set_position (DCPTime::from_frames (7, film->video_frame_rate ()));
+
+	film->make_dcp ();
+
+	wait_for_jobs ();
+
+	boost::filesystem::path ref;
+	ref = "test";
+	ref /= "data";
+	ref /= "black_fill_test";
+
+	boost::filesystem::path check;
+	check = "build";
+	check /= "test";
+	check /= "black_fill_test";
+	check /= film->dcp_name();
+
+	check_dcp (ref.string(), check.string());
 }
