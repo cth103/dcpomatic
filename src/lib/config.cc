56abd650a3e915c398db8dbeaf5260452234ac9f
Carl Hetherington <cth@carlh.net>
2017-04-26 10:13:49 +0100

    Copy config.xml and cinemas.xml if they fail to load so data is not lost.

--- src/lib/config.cc
+++ src/lib/config.cc
@@ -323,6 +323,13 @@ try
 }
 catch (...) {
 	if (have_existing ("config.xml")) {
+
+		/* Make a copy of the configuration */
+		try {
+			boost::filesystem::copy (path ("config.xml", false), path ("config.xml.backup", false));
+			boost::filesystem::copy (path ("cinemas.xml", false), path ("cinemas.xml.backup", false));
+		} catch (...) {}
+
 		/* We have a config file but it didn't load */
 		FailedToLoad ();
 	}
