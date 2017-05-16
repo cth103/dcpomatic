92850876fce46bf3713d5116ec868b321315f7b5
Carl Hetherington <cth@carlh.net>
2017-05-16 14:05:03 +0100

    repeat_frame_test lost one frame of black off the end; never mind.

--- test/repeat_frame_test.cc
+++ test/repeat_frame_test.cc
@@ -53,8 +53,6 @@ BOOST_AUTO_TEST_CASE (repeat_frame_test)
 	film->make_dcp ();
 	wait_for_jobs ();
 
-	/* Should be 31 frames of red; the last one gets replaced with black rather
-	   than repeated.
-	*/
+	/* Should be 31 frames of red */
 	check_dcp ("test/data/repeat_frame_test", film->dir (film->dcp_name ()));
 }
