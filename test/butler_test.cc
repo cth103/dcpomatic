34c3e11437ec32efe8dc4a5be840304ee62f6015
Carl Hetherington <cth@carlh.net>
2017-07-12 23:21:35 +0100

    Missing file.

---
+++ test/butler_test.cc
@@ -0,0 +1,57 @@
+/*
+    Copyright (C) 2017 Carl Hetherington <cth@carlh.net>
+
+    This file is part of DCP-o-matic.
+
+    DCP-o-matic is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    DCP-o-matic is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DCP-o-matic.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "lib/butler.h"
+#include "lib/film.h"
+#include "lib/dcp_content_type.h"
+#include "lib/ratio.h"
+#include "lib/content_factory.h"
+#include "lib/audio_mapping.h"
+#include "lib/player.h"
+#include "test.h"
+#include <boost/test/unit_test.hpp>
+
+using boost::shared_ptr;
+
+BOOST_AUTO_TEST_CASE (butler_test1)
+{
+	shared_ptr<Film> film = new_test_film ("butler_test1");
+	film->set_dcp_content_type (DCPContentType::from_isdcf_name ("FTR"));
+	film->set_name ("butler_test1");
+	film->set_container (Ratio::from_id ("185"));
+
+	shared_ptr<Content> video = content_factory(film, "test/data/flat_red.png").front ();
+	film->examine_and_add_content (video);
+	shared_ptr<Content> audio = content_factory(film, "test/data/staircase.wav").front ();
+	film->examine_and_add_content (audio);
+	BOOST_REQUIRE (!wait_for_jobs ());
+
+	film->set_audio_channels (6);
+	AudioMapping map = AudioMapping (6, 6);
+
+	Butler butler (film, shared_ptr<Player>(new Player(film, film->playlist())), map, 6);
+
+	BOOST_CHECK (butler.get_video().second == DCPTime());
+	BOOST_CHECK (butler.get_video().second == DCPTime::from_frames(1, 24));
+	BOOST_CHECK (butler.get_video().second == DCPTime::from_frames(2, 24));
+
+	float buffer[256 * 6];
+	butler.get_audio(buffer, 256);
+}
