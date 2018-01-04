e21611dbada74946fcdb037386f0e58fad8fc97e
Carl Hetherington <cth@carlh.net>
2018-01-04 20:21:06 +0000

    Supporter.

--- src/wx/about_dialog.cc
+++ src/wx/about_dialog.cc
@@ -177,6 +177,7 @@ AboutDialog::AboutDialog (wxWindow* parent)
 	supported_by.Add (wxT ("Peter Ericson"));
 	supported_by.Add (wxT ("Darius Family"));
 	supported_by.Add (wxT ("Rasitha Ferdinando"));
+	supported_by.Add (wxT ("Santiago Ferreira"));
 	supported_by.Add (wxT ("Jose Angel Velasco Fernandez"));
 	supported_by.Add (wxT ("Traverso City Film Festival"));
 	supported_by.Add (wxT ("Andres Fink"));
