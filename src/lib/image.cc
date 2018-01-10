1d7a98b5c7537b4874645c56cd83ece9f78625f5
Carl Hetherington <cth@carlh.net>
2018-01-10 00:25:47 +0000

    Fix some incorrect alpha blending; may help with #1155.

--- src/lib/image.cc
+++ src/lib/image.cc
@@ -439,7 +439,7 @@ Image::make_transparent ()
 void
 Image::alpha_blend (shared_ptr<const Image> other, Position<int> position)
 {
-	/* We're blending RGBA images; first byte is blue, second byte is green, third byte blue, fourth byte alpha */
+	/* We're blending RGBA images; first byte is red, second byte is green, third byte blue, fourth byte alpha */
 	DCPOMATIC_ASSERT (other->pixel_format() == AV_PIX_FMT_RGBA);
 	int const other_bpp = 4;
 
@@ -469,9 +469,9 @@ Image::alpha_blend (shared_ptr<const Image> other, Position<int> position)
 			uint8_t* op = other->data()[0] + oy * other->stride()[0];
 			for (int tx = start_tx, ox = start_ox; tx < size().width && ox < other->size().width; ++tx, ++ox) {
 				float const alpha = float (op[3]) / 255;
-				tp[0] = op[2] * alpha + tp[0] * (1 - alpha);
+				tp[0] = op[0] * alpha + tp[0] * (1 - alpha);
 				tp[1] = op[1] * alpha + tp[1] * (1 - alpha);
-				tp[2] = op[0] * alpha + tp[2] * (1 - alpha);
+				tp[2] = op[2] * alpha + tp[2] * (1 - alpha);
 
 				tp += this_bpp;
 				op += other_bpp;
@@ -480,6 +480,24 @@ Image::alpha_blend (shared_ptr<const Image> other, Position<int> position)
 		break;
 	}
 	case AV_PIX_FMT_BGRA:
+	{
+		int const this_bpp = 4;
+		for (int ty = start_ty, oy = start_oy; ty < size().height && oy < other->size().height; ++ty, ++oy) {
+			uint8_t* tp = data()[0] + ty * stride()[0] + start_tx * this_bpp;
+			uint8_t* op = other->data()[0] + oy * other->stride()[0];
+			for (int tx = start_tx, ox = start_ox; tx < size().width && ox < other->size().width; ++tx, ++ox) {
+				float const alpha = float (op[3]) / 255;
+				tp[0] = op[2] * alpha + tp[0] * (1 - alpha);
+				tp[1] = op[1] * alpha + tp[1] * (1 - alpha);
+				tp[2] = op[0] * alpha + tp[2] * (1 - alpha);
+				tp[3] = op[3] * alpha + tp[3] * (1 - alpha);
+
+				tp += this_bpp;
+				op += other_bpp;
+			}
+		}
+		break;
+	}
 	case AV_PIX_FMT_RGBA:
 	{
 		int const this_bpp = 4;
@@ -507,10 +525,10 @@ Image::alpha_blend (shared_ptr<const Image> other, Position<int> position)
 			uint8_t* op = other->data()[0] + oy * other->stride()[0];
 			for (int tx = start_tx, ox = start_ox; tx < size().width && ox < other->size().width; ++tx, ++ox) {
 				float const alpha = float (op[3]) / 255;
-				/* Blend high bytes; the RGBA in op appears to be BGRA */
-				tp[1] = op[2] * alpha + tp[1] * (1 - alpha);
+				/* Blend high bytes */
+				tp[1] = op[0] * alpha + tp[1] * (1 - alpha);
 				tp[3] = op[1] * alpha + tp[3] * (1 - alpha);
-				tp[5] = op[0] * alpha + tp[5] * (1 - alpha);
+				tp[5] = op[2] * alpha + tp[5] * (1 - alpha);
 
 				tp += this_bpp;
 				op += other_bpp;
