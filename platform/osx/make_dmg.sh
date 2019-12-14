914108159a15a678d8960880b21f1af92826bf87
Carl Hetherington <cth@carlh.net>
2019-12-15 00:19:16 +0100

    Typo in installation copying.

--- platform/osx/make_dmg.sh
+++ platform/osx/make_dmg.sh
@@ -207,7 +207,7 @@ function copy_resources {
     cp $prefix/src/dcpomatic/graphics/snap.png "$dest"
     cp $prefix/src/dcpomatic/graphics/sequence.png "$dest"
     cp $prefix/src/dcpomatic/graphics/me.jpg "$dest"
-    cp -r $prefix/share/libdcp/sdk "$dest"
+    cp -r $prefix/share/libdcp/xsd "$dest"
 
     # i18n: DCP-o-matic .mo files
     for lang in de_DE es_ES fr_FR it_IT sv_SE nl_NL ru_RU pl_PL da_DK pt_PT pt_BR sk_SK cs_CZ uk_UA zh_CN tr_TR; do
