c93449bb6d866f6ff9cdc94ca9babdfd8b53c870
Carl Hetherington <cth@carlh.net>
2020-01-27 20:27:52 +0100

    Fix missing preferences icon on macOS.

--- platform/osx/make_dmg.sh
+++ platform/osx/make_dmg.sh
@@ -196,6 +196,7 @@ function copy_resources {
     cp $prefix/src/dcpomatic/graphics/osx/preferences/cover_sheet.png "$dest"
     cp $prefix/src/dcpomatic/graphics/osx/preferences/notifications.png "$dest"
     cp $prefix/src/dcpomatic/graphics/osx/preferences/locations.png "$dest"
+    cp $prefix/src/dcpomatic/graphics/osx/preferences/sound.png "$dest"
     cp $prefix/src/dcpomatic/fonts/LiberationSans-Regular.ttf "$dest"
     cp $prefix/src/dcpomatic/fonts/LiberationSans-Italic.ttf "$dest"
     cp $prefix/src/dcpomatic/fonts/LiberationSans-Bold.ttf "$dest"
