143cfcf6c9e8c0a0879652d0d741ec268e8d9541
Carl Hetherington <cth@carlh.net>
2020-04-17 21:59:14 +0200

    Rename a variable.

--- src/lib/writer.cc
+++ src/lib/writer.cc
@@ -214,18 +214,18 @@ Writer::fake_write (Frame frame, Eyes eyes)
 	}
 
 	size_t const reel = video_reel (frame);
-	Frame const reel_frame = frame - _reels[reel].start ();
+	Frame const frame_in_reel = frame - _reels[reel].start ();
 
 	QueueItem qi;
 	qi.type = QueueItem::FAKE;
 
 	{
 		shared_ptr<InfoFileHandle> info_file = _film->info_file_handle(_reels[reel].period(), true);
-		qi.size = _reels[reel].read_frame_info(info_file, reel_frame, eyes).size;
+		qi.size = _reels[reel].read_frame_info(info_file, frame_in_reel, eyes).size;
 	}
 
 	qi.reel = reel;
-	qi.frame = reel_frame;
+	qi.frame = frame_in_reel;
 	if (_film->three_d() && eyes == EYES_BOTH) {
 		qi.eyes = EYES_LEFT;
 		_queue.push_back (qi);
