a98c4b8044f9aa60bb2877dcb13e84852be28371
Carl Hetherington <cth@carlh.net>
2020-06-08 19:41:10 +0200

    Improve look of DCP metadata dialogue on scaling (#1762).

--- src/wx/metadata_dialog.cc
+++ src/wx/metadata_dialog.cc
@@ -61,7 +61,7 @@ MetadataDialog::MetadataDialog (wxWindow* parent, weak_ptr<Film> film)
 
 	vector<EditableListColumn> columns;
 	columns.push_back (EditableListColumn("Agency", 200, true));
-	columns.push_back (EditableListColumn("Label", 50, false));
+	columns.push_back (EditableListColumn("Label", 50, true));
 	_ratings = new EditableList<dcp::Rating, RatingDialog> (
 		this,
 		columns,
