7c5e4e6b7a29db93184a57076d05193a8360561d
Carl Hetherington <cth@carlh.net>
2020-08-03 09:51:43 +0200

    Add unit test for pulldown detection.
    Backported-from-commit: 23804b8beddd616cef60900d6e51deb7788cbd79
    Backported-from-branch: v2.15.x

---
+++ test/pulldown_detect_test.cc
@@ -0,0 +1,41 @@
+/*
+    Copyright (C) 2020 Carl Hetherington <cth@carlh.net>
+
+    This file is part of DCP-o-matic.
+
+    DCP-o-matic is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    DCP-o-matic is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DCP-o-matic.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+#include "test.h"
+#include "lib/content.h"
+#include "lib/content_factory.h"
+#include "lib/film.h"
+#include <boost/test/unit_test.hpp>
+
+
+using boost::shared_ptr;
+
+
+BOOST_AUTO_TEST_CASE (pulldown_detect_test1)
+{
+	shared_ptr<Film> film = new_test_film2 ("pulldown_detect_test1");
+	shared_ptr<Content> content = content_factory(private_data / "greatbrain.mkv").front();
+	film->examine_and_add_content (content);
+	BOOST_REQUIRE (!wait_for_jobs());
+	BOOST_REQUIRE (static_cast<bool>(content->video_frame_rate()));
+	BOOST_CHECK_CLOSE (content->video_frame_rate().get(), 23.976, 0.1);
+}
+
