be4abd363000695f148c36a986160f70dbc659ef
Carl Hetherington <cth@carlh.net>
2020-10-15 13:53:39 +0200

    Test script tweaks for Windows.

--- run/tests.bat
+++ run/tests.bat
@@ -1,3 +1,6 @@
-set PATH=%PATH%;c:\users\ci\bin;c:\users\ci\build\bin
+set PATH=%PATH%;c:\users\ci\bin;c:\users\ci\workspace\dcpomatic\bin
 set DCPOMATIC_TEST_PRIVATE=c:\users\ci\dcpomatic-test-private
+xcopy ..\libdcp\tags build\tags\
+copy ..\openssl\apps\openssl.exe build\test\
+xcopy fonts build\fonts\
 build\test\unit-tests.exe
