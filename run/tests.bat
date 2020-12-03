e54d42a80de7c9a22caa4d0426f4371d83a8dee0
Carl Hetherington <cth@carlh.net>
2020-12-03 21:06:34 +0100

    Add extra path for Windows tests so it can find the DLLs for lwext4.

--- run/tests.bat
+++ run/tests.bat
@@ -1,4 +1,4 @@
-set PATH=%PATH%;c:\users\ci\bin;c:\users\ci\workspace\dcpomatic\bin
+set PATH=%PATH%;c:\users\ci\bin;c:\users\ci\workspace\dcpomatic\bin;c:\users\ci\workspace\dcpomatic\lib
 set DCPOMATIC_TEST_PRIVATE=c:\users\ci\dcpomatic-test-private
 xcopy ..\libdcp\tags build\tags\
 copy ..\openssl\apps\openssl.exe build\test\
