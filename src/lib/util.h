76b74266dabbefef862e03cfb497db27b3b77c7b
Carl Hetherington <cth@carlh.net>
2020-12-04 23:18:25 +0100

    Bump CLOSED_CAPTION_LENGTH to 32 as per SMPTE Bv2.1.

--- src/lib/util.h
+++ src/lib/util.h
@@ -63,8 +63,8 @@ namespace dcp {
 #define MAX_KDM_SIZE (256 * 1024)
 /** Number of lines that closed caption viewers will display */
 #define CLOSED_CAPTION_LINES 3
-/** Maximum line length of closed caption viewers */
-#define CLOSED_CAPTION_LENGTH 30
+/** Maximum line length of closed caption viewers, according to SMPTE Bv2.1 */
+#define CLOSED_CAPTION_LENGTH 32
 
 extern std::string program_name;
 extern bool is_batch_converter;
