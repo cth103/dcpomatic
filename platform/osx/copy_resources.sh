7dc07e0a608c1b41ad63e24edfa8a92836f5be99
Carl Hetherington <cth@carlh.net>
2020-12-08 21:03:07 +0100

    Add partially-working resources copying script for macOS testing.

---
+++ platform/osx/copy_resources.sh
@@ -0,0 +1,4 @@
+mkdir -p build/src/Resources
+cp -r ../libdcp/{tags,xsd} build/src/Resources
+cp graphics/{link,splash}.png build/src/Resources
+cp graphics/osx/preferences/*.png build/src/Resources
