0d517c0cd823e747e3ba1a41dcc59dfae4ed2e0d
Carl Hetherington <cth@carlh.net>
2020-12-30 01:10:45 +0100

    New library.

--- platform/osx/make_dmg.sh
+++ platform/osx/make_dmg.sh
@@ -218,6 +218,7 @@ function copy_libs {
     copy_lib_env libxerces-c "$dest"
     copy_lib_env libcrypto "$dest"
     copy_lib_env libssl "$dest"
+    copy_lib_env libfribidi "$dest"
 }
 
 # @param #1 directory to copy to
