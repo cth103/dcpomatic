dadc58f381cf71468e0218b4f6762e1c3b17dc25
Carl Hetherington <cth@carlh.net>
2021-01-15 12:50:29 +0100

    Symlink openssl when testing.

--- platform/osx/copy_resources.sh
+++ platform/osx/copy_resources.sh
@@ -2,3 +2,4 @@ mkdir -p build/src/Resources
 cp -r ../libdcp/{tags,xsd} build/src/Resources
 cp graphics/{link,splash}.png build/src/Resources
 cp graphics/osx/preferences/*.png build/src/Resources
+ln -s $(which openssl) build/src/tools/openssl
