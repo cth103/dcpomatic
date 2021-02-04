b66b0060a8134c0d00d3df365d8835d5fbe00455
Carl Hetherington <cth@carlh.net>
2021-02-04 23:14:07 +0100

    Fix set_paths.sh to be used on ci.

--- platform/osx/set_paths.sh
+++ platform/osx/set_paths.sh
@@ -1,11 +1,11 @@
-base=$HOME/dcpomatic
+base=$HOME/workspace
 env=$HOME/osx-environment
 sdk=$HOME/SDK
 
-export CPPFLAGS= LDFLAGS="-L$base/lib -L$env/64/lib -isysroot $sdk/MacOSX11.0.sdk -arch x86_64"
-export LINKFLAGS="-L$base/lib -L$env/64/lib -isysroot $sdk/MacOSX11.0.sdk -arch x86_64"
+export CPPFLAGS= LDFLAGS="-L$base/lib -L$env/x86_64/lib -isysroot $sdk/MacOSX11.0.sdk -arch x86_64"
+export LINKFLAGS="-L$base/lib -L$env/x86_64/lib -isysroot $sdk/MacOSX11.0.sdk -arch x86_64"
 export MACOSX_DEPLOYMENT_TARGET=10.10
-export CXXFLAGS="-I$base/include -I$env/64/include -isysroot $sdk/MacOSX11.0.sdk -arch x86_64"
-export CFLAGS="-I$base/include -I$env/64/include -isysroot $sdk/MacOSX11.0.sdk -arch x86_64"
+export CXXFLAGS="-I$base/include -I$env/x86_64/include -isysroot $sdk/MacOSX11.0.sdk -arch x86_64"
+export CFLAGS="-I$base/include -I$env/x86_64/include -isysroot $sdk/MacOSX11.0.sdk -arch x86_64"
 export PATH=$env/x86_64/bin:$PATH
 export PKG_CONFIG_PATH=$env/x86_64/lib/pkgconfig:$base/lib/pkgconfig
