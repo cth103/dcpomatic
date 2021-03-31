9dce1d2646fa086af5006afe65723a50df583788
Carl Hetherington <cth@carlh.net>
2021-03-31 23:13:02 +0200

    Missing part of osx/copy_resources.sh

--- platform/osx/copy_resources.sh
+++ platform/osx/copy_resources.sh
@@ -1,5 +1,6 @@
 mkdir -p build/src/Resources
 cp -r ../libdcp/{tags,xsd} build/src/Resources
 cp graphics/{link,splash}.png build/src/Resources
+cp graphics/{select,snap,sequence,zoom,zoom_all}.png build/src/Resources
 cp graphics/osx/preferences/*.png build/src/Resources
 ln -s $(which openssl) build/src/tools/openssl
