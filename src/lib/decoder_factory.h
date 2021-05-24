5c80bd735ebf02ad476c335adbb8ad9ebd8d27a6
Carl Hetherington <cth@carlh.net>
2021-05-24 23:15:46 +0200

    Forward-declare the right class.

--- src/lib/decoder_factory.h
+++ src/lib/decoder_factory.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2016-2019 Carl Hetherington <cth@carlh.net>
+    Copyright (C) 2016-2021 Carl Hetherington <cth@carlh.net>
 
     This file is part of DCP-o-matic.
 
@@ -18,7 +18,9 @@
 
 */
 
-class ImageDecoder;
+
+class Decoder;
+
 
 extern std::shared_ptr<Decoder> decoder_factory (
 	std::shared_ptr<const Film> film,
