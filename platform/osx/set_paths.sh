6014c99b59acad4586ef033c2ba3500f866f64f2
Carl Hetherington <cth@carlh.net>
2021-06-06 23:33:23 +0200

    Fix paths for changes to environment.

--- platform/osx/set_paths.sh
+++ platform/osx/set_paths.sh
@@ -1,11 +1,11 @@
 base=$HOME/workspace
-env=$HOME/osx-environment
+env=$HOME/osx-environment/x86_64/10.10
 sdk=$HOME/SDK
 
-export CPPFLAGS= LDFLAGS="-L$base/lib -L$env/x86_64/lib -isysroot $sdk/MacOSX11.0.sdk -arch x86_64"
-export LINKFLAGS="-L$base/lib -L$env/x86_64/lib -isysroot $sdk/MacOSX11.0.sdk -arch x86_64"
+export CPPFLAGS= LDFLAGS="-L$base/lib -L$env/lib -isysroot $sdk/MacOSX11.0.sdk -arch x86_64"
+export LINKFLAGS="-L$base/lib -L$env/lib -isysroot $sdk/MacOSX11.0.sdk -arch x86_64"
 export MACOSX_DEPLOYMENT_TARGET=10.10
-export CXXFLAGS="-I$base/include -I$env/x86_64/include -isysroot $sdk/MacOSX11.0.sdk -arch x86_64"
-export CFLAGS="-I$base/include -I$env/x86_64/include -isysroot $sdk/MacOSX11.0.sdk -arch x86_64"
-export PATH=$env/x86_64/bin:$PATH
-export PKG_CONFIG_PATH=$env/x86_64/lib/pkgconfig:$base/lib/pkgconfig
+export CXXFLAGS="-I$base/include -I$env/include -isysroot $sdk/MacOSX11.0.sdk -arch x86_64"
+export CFLAGS="-I$base/include -I$env/include -isysroot $sdk/MacOSX11.0.sdk -arch x86_64"
+export PATH=$env/bin:$PATH
+export PKG_CONFIG_PATH=$env/lib/pkgconfig:$base/lib/pkgconfig
