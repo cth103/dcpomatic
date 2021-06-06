f26f3ee5791440776e847ac96bad4ebeb83055a6
Carl Hetherington <cth@carlh.net>
2021-06-06 12:57:45 +0200

    Cleanup: use bitmap_path().

--- src/tools/dcpomatic_server.cc
+++ src/tools/dcpomatic_server.cc
@@ -217,7 +217,7 @@ public:
 #ifdef DCPOMATIC_WINDOWS
 		wxIcon icon (std_to_wx ("id"));
 #else
-		wxBitmap bitmap (wxString::Format(wxT("%s/dcpomatic_small.png"), std_to_wx(resources_path().string())), wxBITMAP_TYPE_PNG);
+		wxBitmap bitmap (bitmap_path("dcpomatic_small"), wxBITMAP_TYPE_PNG);
 		wxIcon icon;
 		icon.CopyFromBitmap (bitmap);
 #endif
