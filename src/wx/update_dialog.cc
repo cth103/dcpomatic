31de5b1272d928557a7272977584f5dcc9876c75
Carl Hetherington <cth@carlh.net>
2021-06-29 23:36:33 +0200

    Update dialog layout fixes.

--- src/wx/update_dialog.cc
+++ src/wx/update_dialog.cc
@@ -45,27 +45,27 @@ UpdateDialog::UpdateDialog (wxWindow* parent, optional<string> stable, optional<
 		message = new StaticText (this, _("New versions of DCP-o-matic are available."));
 	}
 
-	overall_sizer->Add (message, 1, wxTOP | wxLEFT | wxRIGHT, DCPOMATIC_DIALOG_BORDER);
+	overall_sizer->Add (message, 0, wxTOP | wxLEFT | wxRIGHT, DCPOMATIC_DIALOG_BORDER);
 
 	auto table = new wxFlexGridSizer (2, DCPOMATIC_SIZER_X_GAP, DCPOMATIC_SIZER_Y_GAP);
 
 	if (stable) {
-		add_label_to_sizer (table, this, _("Stable version ") + std_to_wx (stable.get ()), true);
-		table->Add (h);
+		add_label_to_sizer (table, this, _("Stable version ") + std_to_wx(stable.get()), true, 0, wxALIGN_CENTER_VERTICAL);
 		auto h = new wxHyperlinkCtrl (this, wxID_ANY, "dcpomatic.com/download", "https://dcpomatic.com/download");
+		table->Add (h, 0, wxALIGN_CENTER_VERTICAL, DCPOMATIC_DIALOG_BORDER);
 	}
 
 	if (test) {
-		add_label_to_sizer (table, this, _("Test version ") + std_to_wx (test.get ()), true);
-		table->Add (h);
+		add_label_to_sizer (table, this, _("Test version ") + std_to_wx(test.get()), true, 0, wxALIGN_CENTER_VERTICAL);
 		auto h = new wxHyperlinkCtrl (this, wxID_ANY, "dcpomatic.com/test-download", "https://dcpomatic.com/test-download");
+		table->Add (h, 0, wxALIGN_CENTER_VERTICAL, DCPOMATIC_DIALOG_BORDER);
 	}
 
-	overall_sizer->Add (table, 1, wxEXPAND | wxLEFT | wxRIGHT | wxTOP, DCPOMATIC_DIALOG_BORDER);
+	overall_sizer->Add (table, 0, wxTOP | wxLEFT | wxRIGHT, DCPOMATIC_DIALOG_BORDER);
 
 	auto buttons = CreateButtonSizer (wxOK);
 	if (buttons) {
-		overall_sizer->Add (buttons, 1, wxEXPAND | wxALL);
+		overall_sizer->Add (buttons, 1, wxEXPAND | wxALL, DCPOMATIC_DIALOG_BORDER);
 	}
 
 	SetSizerAndFit (overall_sizer);
