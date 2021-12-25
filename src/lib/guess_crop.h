d0cd898b066b9eed3a90fbf1cdf6f600416500c1
Carl Hetherington <cth@carlh.net>
2021-12-26 00:16:45 +0100

    Add guess_crop().

---
+++ src/lib/guess_crop.h
@@ -0,0 +1,34 @@
+/*
+    Copyright (C) 2021 Carl Hetherington <cth@carlh.net>
+
+    This file is part of DCP-o-matic.
+
+    DCP-o-matic is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    DCP-o-matic is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with DCP-o-matic.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+#include "dcpomatic_time.h"
+#include "types.h"
+#include <memory>
+
+
+class Content;
+class Film;
+class Image;
+
+
+Crop guess_crop (std::shared_ptr<const Image> image, double threshold);
+Crop guess_crop (std::shared_ptr<const Film> fillm, std::shared_ptr<const Content> content, double threshold, dcpomatic::ContentTime position);
+
