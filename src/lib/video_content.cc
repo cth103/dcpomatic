cth103/dcpomatic

commit 7ca647ac6ae51893f2714d72816a4449a18e6c57

Rename Fade{In,Out} to VideoFade{In,Out} in metadata XML.

src/lib/video_content.cc
@@ -179,8 +179,8 @@ VideoContent::VideoContent (Content* parent, cxml::ConstNodePtr node, int versio
 	_yuv = node->optional_bool_child("YUV").get_value_or (true);
 
 	if (version >= 32) {
-		_fade_in = node->number_child<Frame> ("FadeIn");
-		_fade_out = node->number_child<Frame> ("FadeOut");
+		_fade_in = node->optional_number_child<Frame>("VideoFadeIn").get_value_or(node->number_child<Frame>("FadeIn"));
+		_fade_out = node->optional_number_child<Frame>("VideoFadeOut").get_value_or(node->number_child<Frame>("FadeOut"));
 	} else {
 		_fade_in = _fade_out = 0;
 	}
@@ -294,8 +294,8 @@ VideoContent::as_xml (xmlpp::Node* node) const
 		_colour_conversion.get().as_xml (node->add_child("ColourConversion"));
 	}
 	node->add_child("YUV")->add_child_text (_yuv ? "1" : "0");
-	node->add_child("FadeIn")->add_child_text (raw_convert<string> (_fade_in));
-	node->add_child("FadeOut")->add_child_text (raw_convert<string> (_fade_out));
+	node->add_child("VideoFadeIn")->add_child_text(raw_convert<string>(_fade_in));
+	node->add_child("VideoFadeOut")->add_child_text(raw_convert<string>(_fade_out));
 	node->add_child("Range")->add_child_text(_range == VideoRange::FULL ? "full" : "video");
 	_pixel_quanta.as_xml(node->add_child("PixelQuanta"));
 	if (_burnt_subtitle_language) {

commit 4af0c52152c2d80e7c3082852c1ecb6eb5543f6a

Fix bug reading fade in/out tags introduced in 7ca647ac6ae51893f2714d72816a4449a18e6c57

src/lib/video_content.cc
@@ -179,8 +179,18 @@ VideoContent::VideoContent (Content* parent, cxml::ConstNodePtr node, int versio
 	_yuv = node->optional_bool_child("YUV").get_value_or (true);
 
 	if (version >= 32) {
-		_fade_in = node->optional_number_child<Frame>("VideoFadeIn").get_value_or(node->number_child<Frame>("FadeIn"));
-		_fade_out = node->optional_number_child<Frame>("VideoFadeOut").get_value_or(node->number_child<Frame>("FadeOut"));
+		auto vfi = node->optional_number_child<Frame>("VideoFadeIn");
+		if (vfi) {
+			_fade_in = *vfi;
+		} else {
+			_fade_in = node->number_child<Frame>("FadeIn");
+		}
+		auto vfo = node->optional_number_child<Frame>("VideoFadeOut");
+		if (vfo) {
+			_fade_out = *vfo;
+		} else {
+			_fade_out = node->number_child<Frame>("FadeOut");
+		}
 	} else {
 		_fade_in = _fade_out = 0;
 	}