302c94e5cea75abac15c55b7bd7b1513f6bcf885
Carl Hetherington <cth@carlh.net>
2022-05-22 21:28:14 +0200

    Add another directory to the copy_resources.sh script.

--- platform/osx/copy_resources.sh
+++ platform/osx/copy_resources.sh
@@ -1,5 +1,5 @@
 mkdir -p build/src/Resources
-cp -r ../libdcp/{tags,xsd} build/src/Resources
+cp -r ../libdcp/{tags,xsd,ratings} build/src/Resources
 cp graphics/{link,splash}.png build/src/Resources
 cp graphics/{select,snap,sequence,zoom,zoom_all}.png build/src/Resources
 cp graphics/osx/preferences/*.png build/src/Resources
