a6507c95d19b434275e2a28f0bc37f06912f467f
Carl Hetherington <cth@carlh.net>
2022-07-05 23:10:01 +0200

    Add useful debugging test which might be tricky to use properly because of rendering differences.

--- test/render_subtitles_test.cc
+++ test/render_subtitles_test.cc
@@ -25,10 +25,15 @@
  */
 
 
+#include "lib/image.h"
+#include "lib/image_png.h"
 #include "lib/render_text.h"
 #include "lib/string_text.h"
 #include <dcp/subtitle_string.h>
 #include <boost/test/unit_test.hpp>
+
+
+using std::shared_ptr;
 
 
 static void
@@ -111,3 +116,30 @@ BOOST_AUTO_TEST_CASE (marked_up_test6)
 	add (s, "we are bold.", false, true, false);
 	BOOST_CHECK_EQUAL (marked_up(s, 1024, 1, ""), "<span style=\"italic\" size=\"41705\" alpha=\"65535\" color=\"#FFFFFF\">Hello</span><span size=\"41705\" alpha=\"65535\" color=\"#FFFFFF\"> world </span><span weight=\"bold\" size=\"41705\" alpha=\"65535\" color=\"#FFFFFF\">we are bold.</span>");
 }
+
+
+#if 0
+
+BOOST_AUTO_TEST_CASE (render_text_test)
+{
+	auto dcp_string = dcp::SubtitleString(
+		{}, false, false, false, dcp::Colour(255, 255, 255), 42, 1.0,
+		dcp::Time(0, 0, 0, 0, 24), dcp::Time(0, 0, 1, 0, 24),
+		0.5, dcp::HAlign::CENTER,
+		0.5, dcp::VAlign::CENTER,
+		dcp::Direction::LTR,
+		"HÄllo jokers",
+		dcp::Effect::NONE, dcp::Colour(0, 0, 0),
+		{}, {},
+		0
+		);
+
+	auto string_text = StringText(dcp_string, 0, shared_ptr<dcpomatic::Font>());
+
+	auto images = render_text({ string_text }, dcp::Size(1998, 1080), {}, 24);
+
+	BOOST_CHECK_EQUAL(images.size(), 1U);
+	image_as_png(Image::ensure_alignment(images.front().image, Image::Alignment::PADDED)).write("build/test/render_text_test.png");
+}
+
+#endif
