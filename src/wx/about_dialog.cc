f64f48051c9054830561a868a320b8003126134a
Carl Hetherington <cth@carlh.net>
2022-07-09 23:38:56 +0200

    New sl_SI translation from Martin Srebotnjak.

--- src/wx/about_dialog.cc
+++ src/wx/about_dialog.cc
@@ -142,6 +142,7 @@ AboutDialog::AboutDialog (wxWindow* parent)
 	translated_by.Add (wxT ("Tiago Casal Ribeiro"));
 	translated_by.Add (wxT ("Davide Sanvito"));
 	translated_by.Add (wxT ("Marek Skrzelowski"));
+	translated_by.Add (wxT ("Martin Srebotnjak"));
 	translated_by.Add (wxT ("Michał Tomaszewski"));
 	translated_by.Add (wxT ("Igor Voytovich"));
 	translated_by.Add (wxT ("Rov (若文)"));
