47667725371a394167e602a0aac8e55949e8a39c
Carl Hetherington <cth@carlh.net>
2022-11-06 22:00:55 +0100

    Cleanup: J2KEncoder does not need enable_shared_from_this.

--- src/lib/j2k_encoder.h
+++ src/lib/j2k_encoder.h
@@ -56,7 +56,7 @@ class PlayerVideo;
  *  This class keeps a queue of frames to be encoded and distributes
  *  the work around threads and encoding servers.
  */
-class J2KEncoder : public ExceptionStore, public std::enable_shared_from_this<J2KEncoder>
+class J2KEncoder : public ExceptionStore
 {
 public:
 	J2KEncoder(std::shared_ptr<const Film> film, Writer& writer);
