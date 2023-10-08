e6822e23c6243dc09f12ceb8abf985278e90751a
Carl Hetherington <cth@carlh.net>
2023-10-08 15:48:58 +0200

    Make stop() private and use drop() instead.
    This avoids confusing situations where we call stop(), then try
    to restart the finder (unsuccessfully, because once it's stop()ped
    it will stay around, never to be restarted).

--- test/test.cc
+++ test/test.cc
@@ -161,7 +161,7 @@ struct TestConfig
 		setup_test_config ();
 		capture_ffmpeg_logs();
 
-		EncodeServerFinder::instance()->stop ();
+		EncodeServerFinder::drop();
 
 		signal_manager = new TestSignalManager ();
 
