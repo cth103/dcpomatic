cth103/dcpomatic

commit d3455851f60ee6bc35ea4f314c62a7da51ce0d81

Use _{start,finish}_time in TranscodeJob.

src/lib/transcode_job.cc
@@ -119,19 +119,14 @@ TranscodeJob::run ()
 			}
 		}
 
-		struct timeval start;
-		gettimeofday (&start, 0);
 		LOG_GENERAL_NC (N_("Transcode job starting"));
 
 		DCPOMATIC_ASSERT (_encoder);
 		_encoder->go ();
 
-		struct timeval finish;
-		gettimeofday (&finish, 0);
-
 		float fps = 0;
-		if (finish.tv_sec != start.tv_sec) {
-			fps = _encoder->frames_done() / (finish.tv_sec - start.tv_sec);
+		if (_finish_time != _start_time) {
+			fps = _encoder->frames_done() / (_finish_time - _start_time);
 		}
 
 		LOG_GENERAL (N_("Transcode job completed successfully: %1 fps"), dcp::locale_convert<string>(fps, 2, true));

src/lib/job.cc
@@ -57,7 +57,6 @@ using namespace dcpomatic;
 Job::Job (shared_ptr<const Film> film)
 	: _film (film)
 	, _state (NEW)
-	, _start_time (0)
 	, _sub_start_time (0)
 	, _progress (0)
 {

src/lib/job.h
@@ -130,6 +130,9 @@ protected:
 
 	std::shared_ptr<const Film> _film;
 
+	time_t _start_time = 0;
+	time_t _finish_time = 0;
+
 private:
 
 	void run_wrapper ();
@@ -147,12 +150,9 @@ private:
 	/** a message that should be given to the user when the job finishes */
 	boost::optional<std::string> _message;
 
-	/** time that this job was started */
-	time_t _start_time;
 	/** time that this sub-job was started */
 	time_t _sub_start_time;
 	std::string _sub_name;
-	time_t _finish_time = 0;
 
 	/** mutex for _progress and _last_progress_update */
 	mutable boost::mutex _progress_mutex;

commit 6e1c0f245a3940da7da175a978c0561711cfb447

Fix transcode job fps logging.
Broken in d3455851f60ee6bc35ea4f314c62a7da51ce0d81

src/lib/transcode_job.cc
@@ -124,6 +124,9 @@ TranscodeJob::run ()
 		DCPOMATIC_ASSERT (_encoder);
 		_encoder->go ();
 
+		set_progress (1);
+		set_state (FINISHED_OK);
+
 		LOG_GENERAL(N_("Transcode job completed successfully: %1 fps"), dcp::locale_convert<string>(frames_per_second(), 2, true));
 
 		if (dynamic_pointer_cast<DCPEncoder>(_encoder)) {
@@ -137,9 +140,6 @@ TranscodeJob::run ()
 		post_transcode ();
 
 		_encoder.reset ();
-
-		set_progress (1);
-		set_state (FINISHED_OK);
 
 	} catch (...) {
 		_encoder.reset ();