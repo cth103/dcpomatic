0e18e7acd23ef466cc14a8b2d9a19df4dd991ddc
Carl Hetherington <cth@carlh.net>
2024-02-10 16:49:35 +0100

    Add missing call to get_offsets().

--- src/wx/kdm_timing_panel.cc
+++ src/wx/kdm_timing_panel.cc
@@ -122,7 +122,7 @@ KDMTimingPanel::KDMTimingPanel (wxWindow* parent)
 	_warning->SetFont(font);
 
 	/* Default to UTC */
-	size_t sel = 0;
+	size_t sel = get_offsets(_offsets);
 	for (size_t i = 0; i < _offsets.size(); ++i) {
 		_utc_offset->add(_offsets[i].name);
 		if (_offsets[i].hour == 0 && _offsets[i].minute == 0) {
