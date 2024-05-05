f5e08d6f36161a980682dd3cb9b0678d44adadfd
Carl Hetherington <cth@carlh.net>
2024-05-05 21:34:22 +0200

    Add some missing includes.

--- src/lib/writer.h
+++ src/lib/writer.h
@@ -34,6 +34,8 @@
 #include "exception_store.h"
 #include "font_id_map.h"
 #include "player_text.h"
+#include "text_type.h"
+#include "types.h"
 #include "weak_film.h"
 #include <dcp/atmos_frame.h>
 #include <boost/thread.hpp>
