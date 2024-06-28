6467d7542585b0d61f30c4a87ec93db1d52cc2f5
Carl Hetherington <cth@carlh.net>
2024-06-28 16:53:00 +0200

    Fix test build.

--- test/subtitle_font_id_test.cc
+++ test/subtitle_font_id_test.cc
@@ -337,7 +337,7 @@ BOOST_AUTO_TEST_CASE(cope_with_unloaded_font_id)
 {
 	/* This file has a <Font> with an ID that corresponds to no <LoadFont> */
 	auto subs = content_factory("test/data/unloaded_font.xml")[0];
-	auto film = new_test_film2("cope_with_unloaded_font_id", { subs });
+	auto film = new_test_film("cope_with_unloaded_font_id", { subs });
 
 	make_and_verify_dcp(
 		film,
