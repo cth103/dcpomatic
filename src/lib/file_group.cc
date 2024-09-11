cth103/dcpomatic

commit 5db3eda9c6f0d6b027263c519000997677253504

Fix confusion about operator bool() with optional.
I think previously no exception would be thrown even if the file weren't
open.

src/lib/file_group.cc
@@ -84,11 +84,13 @@ FileGroup::ensure_open_path (size_t p) const
 		_current_file->close();
 	}
 
-	_current_path = p;
-	_current_file = dcp::File(_paths[_current_path], "rb");
-	if (!_current_file) {
-		throw OpenFileError (_paths[_current_path], errno, OpenFileError::READ);
+	auto file = dcp::File(_paths[_current_path], "rb");
+	if (!file) {
+		throw OpenFileError(_paths[_current_path], errno, OpenFileError::READ);
 	}
+
+	_current_path = p;
+	_current_file = std::move(file);
 	_current_size = dcp::filesystem::file_size(_paths[_current_path]);
 }
 

commit 179e362688a5a533f771942cde5126c0b11c7d08

Fix stupid bug introduced in 5db3eda9c6f0

src/lib/file_group.cc
@@ -84,9 +84,9 @@ FileGroup::ensure_open_path (size_t p) const
 		_current_file->close();
 	}
 
-	auto file = dcp::File(_paths[_current_path], "rb");
+	auto file = dcp::File(_paths[p], "rb");
 	if (!file) {
-		throw OpenFileError(_paths[_current_path], file.open_error(), OpenFileError::READ);
+		throw OpenFileError(_paths[p], file.open_error(), OpenFileError::READ);
 	}
 
 	_current_path = p;