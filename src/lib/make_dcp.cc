4ffe0ef9fe7fe2e636f5c8dd247abc0be19d508c
Carl Hetherington <cth@carlh.net>
2025-12-30 01:09:35 +0100

    White space: make_dcp.cc

--- src/lib/make_dcp.cc
+++ src/lib/make_dcp.cc
@@ -42,56 +42,56 @@ using std::string;
 
 /** Add suitable Job to the JobManager to create a DCP for a Film */
 shared_ptr<TranscodeJob>
-make_dcp (shared_ptr<Film> film, TranscodeJob::ChangedBehaviour behaviour)
+make_dcp(shared_ptr<Film> film, TranscodeJob::ChangedBehaviour behaviour)
 {
 	if (film->dcp_name().find("/") != string::npos) {
-		throw BadSettingError (_("name"), _("Cannot contain slashes"));
+		throw BadSettingError(_("name"), _("Cannot contain slashes"));
 	}
 
 	if (film->content().empty()) {
-		throw runtime_error (_("You must add some content to the DCP before creating it"));
+		throw runtime_error(_("You must add some content to the DCP before creating it"));
 	}
 
 	if (film->length() == dcpomatic::DCPTime()) {
-		throw runtime_error (_("The DCP is empty, perhaps because all the content has zero length."));
+		throw runtime_error(_("The DCP is empty, perhaps because all the content has zero length."));
 	}
 
 	if (film->dcp_content_type() == nullptr) {
-		throw MissingSettingError (_("content type"));
+		throw MissingSettingError(_("content type"));
 	}
 
 	if (film->name().empty()) {
-		film->set_name ("DCP");
+		film->set_name("DCP");
 	}
 
 	for (auto i: film->content()) {
 		if (!paths_exist(i->paths())) {
-			throw runtime_error (_("Some of your content is missing"));
+			throw runtime_error(_("Some of your content is missing"));
 		}
 		auto dcp = dynamic_pointer_cast<const DCPContent>(i);
 		if (dcp && dcp->needs_kdm()) {
-			throw runtime_error (_("Some of your content needs a KDM"));
+			throw runtime_error(_("Some of your content needs a KDM"));
 		}
 		if (dcp && dcp->needs_assets()) {
-			throw runtime_error (_("Some of your content needs an OV"));
+			throw runtime_error(_("Some of your content needs an OV"));
 		}
 	}
 
-	film->set_isdcf_date_today ();
+	film->set_isdcf_date_today();
 
 	for (auto info: environment_info()) {
-		LOG_GENERAL (info);
+		LOG_GENERAL(info);
 	}
 
 	for (auto content: film->content()) {
-		LOG_GENERAL ("Content: {}", content->technical_summary());
+		LOG_GENERAL("Content: {}", content->technical_summary());
 	}
-	LOG_GENERAL ("DCP video rate {} fps", film->video_frame_rate());
+	LOG_GENERAL("DCP video rate {} fps", film->video_frame_rate());
 	LOG_GENERAL("Video bit rate {}", film->video_bit_rate(film->video_encoding()));
 
 	auto tj = make_shared<DCPTranscodeJob>(film, behaviour);
 	tj->set_encoder(make_shared<DCPFilmEncoder>(film, tj));
-	JobManager::instance()->add (tj);
+	JobManager::instance()->add(tj);
 
 	return tj;
 }
