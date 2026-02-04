d424ea6b6aa710c5384e91d8e9e343947587a00b
Carl Hetherington <cth@carlh.net>
2026-02-04 17:52:33 +0100

    Copy web resources on macOS.

--- platform/osx/copy_resources.sh
+++ platform/osx/copy_resources.sh
@@ -1,4 +1,5 @@
 mkdir -p build/src/Resources
 cp -r ../libdcp/{tags,xsd,ratings} build/src/Resources
 cp fonts/*.ttf build/src/Resources
+cp -r web build/src/Resources
 ln -s $(which openssl) build/src/tools/openssl
