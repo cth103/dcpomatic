0afcc3d371a297dc03722ea9a516b6453bcfcb4e
Carl Hetherington <cth@carlh.net>
2026-03-22 22:52:20 +0100

    Remove now-unnecessary SHA1 variable.
    I moved it to libdcp.

--- src/lib/util.cc
+++ src/lib/util.cc
@@ -490,12 +490,6 @@ LIBDCP_ENABLE_WARNINGS
 	render_text(subs, dcp::Size(640, 480), DCPTime(), 24);
 #endif
 
-#ifdef DCPOMATIC_WINDOWS
-	putenv("OPENSSL_ENABLE_SHA1_SIGNATURES=1");
-#else
-	setenv("OPENSSL_ENABLE_SHA1_SIGNATURES", "1", 1);
-#endif
-
 	Ratio::setup_ratios();
 	PresetColourConversion::setup_colour_conversion_presets();
 	DCPContentType::setup_dcp_content_types();
