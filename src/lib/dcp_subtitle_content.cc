0f42191dba5596c853ad37714c623fa6bc294d95
Carl Hetherington <cth@carlh.net>
2026-04-16 20:29:55 +0200

    Compress an if.

--- src/lib/dcp_subtitle_content.cc
+++ src/lib/dcp_subtitle_content.cc
@@ -64,8 +64,7 @@ DCPSubtitleContent::examine(shared_ptr<const Film> film, shared_ptr<Job> job, bo
 
 	auto subtitle_asset = load(path(0));
 
-	auto smpte = dynamic_pointer_cast<dcp::SMPTETextAsset>(subtitle_asset);
-	if (smpte) {
+	if (auto smpte = dynamic_pointer_cast<dcp::SMPTETextAsset>(subtitle_asset)) {
 		set_video_frame_rate(film, smpte->edit_rate().numerator);
 	}
 
