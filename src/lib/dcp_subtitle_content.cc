f1a7f3bcafa9f76d080e53d644058904846b877d
Carl Hetherington <cth@carlh.net>
2026-04-16 20:29:16 +0200

    Remove unused variable.

--- src/lib/dcp_subtitle_content.cc
+++ src/lib/dcp_subtitle_content.cc
@@ -64,7 +64,6 @@ DCPSubtitleContent::examine(shared_ptr<const Film> film, shared_ptr<Job> job, bo
 
 	auto subtitle_asset = load(path(0));
 
-	auto iop = dynamic_pointer_cast<dcp::InteropTextAsset>(subtitle_asset);
 	auto smpte = dynamic_pointer_cast<dcp::SMPTETextAsset>(subtitle_asset);
 	if (smpte) {
 		set_video_frame_rate(film, smpte->edit_rate().numerator);
