85fe360bc23bb5f8c1900863adb8b2fb9495f6df
Carl Hetherington <cth@carlh.net>
2026-07-09 23:55:03 +0200

    Add contains() for another TimePeriod.

--- src/lib/dcpomatic_time.h
+++ src/lib/dcpomatic_time.h
@@ -331,6 +331,10 @@ public:
 		return (from <= other && other < to);
 	}
 
+	bool contains(TimePeriod<T> const& other) const {
+		return from <= other.from && to >= other.to;
+	}
+
 	bool operator<(TimePeriod<T> const & o) const {
 		if (from != o.from) {
 			return from < o.from;
