8a2cda4a5f2651383bfb71142dc5d1fdb44f0b9f
Carl Hetherington <cth@carlh.net>
2026-07-20 20:50:45 +0200

    Add Response_400.

--- src/lib/response.cc
+++ src/lib/response.cc
@@ -28,6 +28,7 @@ using std::shared_ptr;
 using std::string;
 
 
+Response Response::ERROR_400 = { 400, "<html><head><title>Error 400</title></head><body><h1>Error 400</h1></body></html>"};
 Response Response::ERROR_404 = { 404, "<html><head><title>Error 404</title></head><body><h1>Error 404</h1></body></html>"};
 Response Response::ERROR_500 = { 500, "<html><head><title>Error 500</title></head><body><h1>Error 500</h1></body></html>"};
 
